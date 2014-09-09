cb619c9da73b91b283ef30adeefa36485b045056
Josh Gough <jsgough@gmail.com>
2014-09-09 12:05:42 -0400

    S-45195  Use port 80 by default

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 var cors = require('cors');
-var port = process.env.PORT || config.port || 80;
+var port = Number(process.env.PORT) || 80;
 
 app.use(cors());
 app.use(express.static(__dirname + '/client'));
