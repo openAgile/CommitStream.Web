22da58c1a9643d386032e9b9c4ad5b4ea198e4f9
Josh Gough <jsgough@gmail.com>
2014-09-09 12:06:52 -0400

    S-45195  Remove cors to see if it's blocking success

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var app = express();
-var cors = require('cors');
+//var cors = require('cors');
 var port = Number(process.env.PORT) || 80;
 
-app.use(cors());
+//app.use(cors());
 app.use(express.static(__dirname + '/client'));
 
 app.listen(port, function () {
