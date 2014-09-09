d1e6bcecb666bd5c8a1c7325a498811c9879e4fb
Josh Gough <jsgough@gmail.com>
2014-09-09 11:35:02 -0400

    S-47665 Pulling in JavaScript for weaving the commit data into asset detail

---
+++ client/assetDetailCommits.js
@@ -0,0 +1,27 @@
+// TODO: remove hard coding:
+var url = 'http://localhost:2113/streams/asset-S-12345/head/backward/5?embed=content';
+var commits = [];
+$.getJSON(url).done(function(events) {
+  $.each(events.entries, function(index, value) {
+    var e = value.content.data;
+    var c = {
+      timeFormatted: moment(e.commit.committer.date).fromNow(),
+      author: e.commit.committer.name,
+      sha1Partial: e.commit.tree.sha.substring(0,6),
+      action: "committed",
+      message: e.commit.message,
+      commitHref: e.html_url
+    };
+    commits.push(c);
+  });
+  var data = {
+    commits: commits
+  };
+  if (commits.length > 0) {
+    $.get("http://localhost/content/githubCommit-template.html").done(function(source) {
+      var template = Handlebars.compile(source);
+      var content = template(data);
+      $("#${csID}").html(content);
+    });
+  }
+});
