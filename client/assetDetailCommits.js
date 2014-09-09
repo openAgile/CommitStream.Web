1d3345a1035183e1138882f90e8947b86ab217b3
Josh Gough <jsgough@gmail.com>
2014-09-09 14:28:20 -0400

    S-45195 Encapsulate assetDetailCommits into a function

--- client/assetDetailCommits.js
+++ client/assetDetailCommits.js
@@ -1,27 +1,27 @@
-// TODO: remove hard coding:
-var url = 'http://localhost:2113/streams/asset-S-12345/head/backward/5?embed=content';
-var commits = [];
-$.getJSON(url).done(function(events) {
-  $.each(events.entries, function(index, value) {
-    var e = value.content.data;
-    var c = {
-      timeFormatted: moment(e.commit.committer.date).fromNow(),
-      author: e.commit.committer.name,
-      sha1Partial: e.commit.tree.sha.substring(0,6),
-      action: "committed",
-      message: e.commit.message,
-      commitHref: e.html_url
-    };
-    commits.push(c);
-  });
-  var data = {
-    commits: commits
-  };
-  if (commits.length > 0) {
-    $.get("http://v1commitstream.azurewebsites.net/assetDetailCommits.html").done(function(source) {
-      var template = Handlebars.compile(source);
-      var content = template(data);
-      $(CommitStreamPanel).html(content);
+function assetDetailCommits(eventStoreQueryUrl, templateUrl, selector) {
+  var commits = [];
+  $.getJSON(eventStoreQueryUrl).done(function(events) {
+    $.each(events.entries, function(index, value) {
+      var e = value.content.data;
+      var c = {
+        timeFormatted: moment(e.commit.committer.date).fromNow(),
+        author: e.commit.committer.name,
+        sha1Partial: e.commit.tree.sha.substring(0,6),
+        action: "committed",
+        message: e.commit.message,
+        commitHref: e.html_url
+      };
+      commits.push(c);
     });
-  }
-});
+    var data = {
+      commits: commits
+    };
+    if (commits.length > 0) {
+      $.get(templateUrl).done(function(source) {
+        var template = Handlebars.compile(source);
+        var content = template(data);
+        $(selector).html(content);
+      });
+    }
+  });
+}
