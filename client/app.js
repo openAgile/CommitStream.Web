010c7b4d1d04e4b6f46efec4e26125cca517803d
Josh Gough <jsgough@gmail.com>
2014-09-11 16:48:54 -0400

    S-47665 Get protocol from window.location.protocol

--- client/app.js
+++ client/app.js
@@ -1,17 +1,20 @@
-require.config({
-    paths: {
-      moment: '//cdnjs.cloudflare.com/ajax/libs/moment.js/2.8.2/moment.min.js',
-      handlebars: '//cdnjs.cloudflare.com/ajax/libs/handlebars.js/2.0.0-alpha.4/handlebars.amd.min.js'
-    },
-    config: {
-        moment: {
-            noGlobal: true
+(function() {
+    var protocol = window.location.protocol + "//";
+    require.config({
+        paths: {
+          moment: protocol + 'cdnjs.cloudflare.com/ajax/libs/moment.js/2.8.2/moment.min.js',
+          handlebars: protocol + 'cdnjs.cloudflare.com/ajax/libs/handlebars.js/2.0.0-alpha.4/handlebars.amd.min.js'
+        },
+        config: {
+            moment: {
+                noGlobal: true
+            }
         }
-    }
-});
-
-require(['handlebars', 'moment'], function (handlebars, moment) {
-  console.log("Got dependencies:");
-  console.log(handlebars.compile);
-  console.log(moment);
-});
+    });
+    
+    require(['handlebars', 'moment'], function (handlebars, moment) {
+      console.log("Got dependencies:");
+      console.log(handlebars.compile);
+      console.log(moment);
+    });
+})();
