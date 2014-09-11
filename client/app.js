c4e6555e5bee3dd5945b104b189167887f0df2e7
Josh Gough <jsgough@gmail.com>
2014-09-11 16:52:27 -0400

    S-47665 Remove .js since require appends it

--- client/app.js
+++ client/app.js
@@ -2,8 +2,8 @@
     var protocol = window.location.protocol + "//";
     require.config({
         paths: {
-          moment: protocol + 'cdnjs.cloudflare.com/ajax/libs/moment.js/2.8.2/moment.min.js',
-          handlebars: protocol + 'cdnjs.cloudflare.com/ajax/libs/handlebars.js/2.0.0-alpha.4/handlebars.amd.min.js'
+          moment: protocol + 'cdnjs.cloudflare.com/ajax/libs/moment.js/2.8.2/moment.min',
+          handlebars: protocol + 'cdnjs.cloudflare.com/ajax/libs/handlebars.js/2.0.0-alpha.4/handlebars.amd.min'
         },
         config: {
             moment: {
