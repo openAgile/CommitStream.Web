dd39470379ccbcb439569be94664527debc63f8f
Josh Gough <jsgough@gmail.com>
2014-09-11 17:01:09 -0400

    S-47665 Try just moment and not handlebars for now

--- client/app.js
+++ client/app.js
@@ -1,10 +1,9 @@
 (function() {
-    //var protocol = window.location.protocol + "//";
     var protocol = "//";
     require.config({
         paths: {
           moment: protocol + 'cdnjs.cloudflare.com/ajax/libs/moment.js/2.8.2/moment.min',
-          handlebars: protocol + 'cdnjs.cloudflare.com/ajax/libs/handlebars.js/2.0.0-alpha.4/handlebars.amd.min'
+          //handlebars: protocol + 'cdnjs.cloudflare.com/ajax/libs/handlebars.js/2.0.0-alpha.4/handlebars.amd.min'
         },
         config: {
             moment: {
@@ -13,9 +12,8 @@
         }
     });
     
-    require(['handlebars', 'moment'], function (handlebars, moment) {
+    require(['moment'], function (moment) {
       console.log("Got dependencies:");
-      console.log(handlebars.compile);
       console.log(moment);
     });
 })();
