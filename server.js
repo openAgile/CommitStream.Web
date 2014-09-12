aebad14e4a6e61b935c8b048f2c56be514121047
Josh Gough <jsgough@gmail.com>
2014-09-12 09:12:04 -0400

    S-47665 Show API link in headers

--- server.js
+++ server.js
@@ -3,14 +3,15 @@ var express = require('express'),
     cors = require('cors'),
     port = process.env.PORT || 80;
 
-app.get('/version', function(req, res) {
-    res.json({version:"0.0.0"});
-});
-
-app.use(cors());
-
-app.use(express.static(__dirname + '/client'));
-
-app.listen(port, function () {
-    console.log('CommitStream Web Server listening on port ' + port);
+app.configure(function(){
+    app.use(cors());
+    app.use(function(req, res, next) {
+        res.setHeader("X-API-Docs", "https://github.com/eventstore/eventstore/wiki");
+        return next();
+    });
+    app.use(express.static(__dirname + '/client'));
+    app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+    app.listen(port, function () {
+        console.log('CommitStream Web Server listening on port ' + port);
+    });
 });
