f11d73f065ebc3f6a3ff0a50ea62083f803f217a
Josh Gough <jsgough@gmail.com>
2014-09-22 16:43:47 -0400

    S-47665 beautified JS

--- client/assetDetailCommitsModule.js
+++ client/assetDetailCommitsModule.js
@@ -1,35 +1,35 @@
 define(['moment'], function(moment) {
-	var apiBaseUrl 	= 'http://v1eventstore.cloudapp.net:2113/streams/asset-',
-			apiParams	 	= '/head/backward/5?embed=content',
-			templateUrl	= 'http://v1commitstream.azurewebsites.net/assetDetailCommits.html';
-			
-	return function(selector, assetNumber) {
-    var commits = [];
-    var apiUrl = apiBaseUrl + assetNumber + apiParams;
-    
-    $.getJSON(apiUrl).done(function(events) {
-      $.each(events.entries, function(index, value) {
-        var e = value.content.data;
-        var c = {
-          timeFormatted: moment(e.commit.committer.date).fromNow(),
-          author: e.commit.committer.name,
-          sha1Partial: e.commit.tree.sha.substring(0,6),
-          action: "committed",
-          message: e.commit.message,
-          commitHref: e.html_url
-        };
-        commits.push(c);
-      });
-      var data = {
-        commits: commits
-      };
-      if (commits.length > 0) {
-        $.get(templateUrl).done(function(source) {
-          var template = Handlebars.compile(source);
-          var content = template(data);
-          $(selector).html(content);
+    var apiBaseUrl = 'http://v1eventstore.cloudapp.net:2113/streams/asset-',
+        apiParams = '/head/backward/5?embed=content',
+        templateUrl = 'http://v1commitstream.azurewebsites.net/assetDetailCommits.html';
+
+    return function(selector, assetNumber) {
+        var commits = [];
+        var apiUrl = apiBaseUrl + assetNumber + apiParams;
+
+        $.getJSON(apiUrl).done(function(events) {
+            $.each(events.entries, function(index, value) {
+                var e = value.content.data;
+                var c = {
+                    timeFormatted: moment(e.commit.committer.date).fromNow(),
+                    author: e.commit.committer.name,
+                    sha1Partial: e.commit.tree.sha.substring(0, 6),
+                    action: "committed",
+                    message: e.commit.message,
+                    commitHref: e.html_url
+                };
+                commits.push(c);
+            });
+            var data = {
+                commits: commits
+            };
+            if (commits.length > 0) {
+                $.get(templateUrl).done(function(source) {
+                    var template = Handlebars.compile(source);
+                    var content = template(data);
+                    $(selector).html(content);
+                });
+            }
         });
-      }
-    });
-  }
+    }
 });
