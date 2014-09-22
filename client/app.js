31c9500cbf1dacfd4b98b5a00477bf462d9c43ad
Josh Gough <jsgough@gmail.com>
2014-09-22 17:13:51 -0400

    S-47665 try to load handlebars ourselves

--- client/app.js
+++ client/app.js
@@ -3,8 +3,8 @@
     require.config({
         paths: {
           moment: protocol + 'cdnjs.cloudflare.com/ajax/libs/moment.js/2.8.2/moment.min',
-          assetDetailCommits: protocol + 'v1commitstream.azurewebsites.net/assetDetailCommitsModule'
-          //handlebars: protocol + 'cdnjs.cloudflare.com/ajax/libs/handlebars.js/2.0.0-alpha.4/handlebars.amd.min'
+          assetDetailCommits: protocol + 'v1commitstream.azurewebsites.net/assetDetailCommitsModule',
+          handlebars: protocol + 'cdnjs.cloudflare.com/ajax/libs/handlebars.js/2.0.0-alpha.4/handlebars.amd.min'
         },
         config: {
             moment: {
