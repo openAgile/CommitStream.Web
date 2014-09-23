56a8849efe819f1979910c77fc5b82f8669c5c43
Josh Gough <jsgough@gmail.com>
2014-09-23 18:23:41 -0400

    S-47665 better URL encapsulation within the js module

--- client/app.js
+++ client/app.js
@@ -1,15 +1,24 @@
+// Put a commitStream object in the global space
+CommitStream = {};
 (function() {
     var protocol = "//";
-    require.config({
-        paths: {
-          moment: protocol + 'cdnjs.cloudflare.com/ajax/libs/moment.js/2.8.2/moment.min',
-          assetDetailCommits: protocol + 'v1commitstream.azurewebsites.net/assetDetailCommitsModule',
-          handlebars: protocol + 'cdnjs.cloudflare.com/ajax/libs/handlebars.js/2.0.0-alpha.4/handlebars.amd.min'
-        },
-        config: {
-            moment: {
-                noGlobal: true
+    $.getScript(protocol + "cdnjs.cloudflare.com/ajax/libs/require.js/2.1.14/require.min.js", function(data, status, jqxhr) {
+        require.config({
+            paths: {
+              moment: protocol + 'cdnjs.cloudflare.com/ajax/libs/moment.js/2.8.2/moment.min',
+              assetDetailCommits: protocol + 'v1commitstream.azurewebsites.net/assetDetailCommitsModule',
+              handlebars: protocol + 'cdnjs.cloudflare.com/ajax/libs/handlebars.js/2.0.0-alpha.4/handlebars.amd.min'
+            },
+            config: {
+                moment: {
+                    noGlobal: true
+                }
             }
-        }
+        });
+        CommitStream.commitsDisplay = function(commitStreamDomId, assetNumber) {
+    		require(['assetDetailCommits'], function(assetDetailCommits) {
+                assetDetailCommits(commitStreamDomId, assetNumber);
+            });            
+        };
     });
 })();
