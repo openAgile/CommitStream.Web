f4ee5d56e833900915825c74244bdc6b850d974a
Josh Gough <jsgough@gmail.com>
2014-09-23 19:00:06 -0400

    S-47665 beautified JS

--- client/app.js
+++ client/app.js
@@ -7,9 +7,9 @@
             $.getScript(protocol + "cdnjs.cloudflare.com/ajax/libs/require.js/2.1.14/require.min.js", function(data, status, jqxhr) {
                 require.config({
                     paths: {
-                      moment: protocol + 'cdnjs.cloudflare.com/ajax/libs/moment.js/2.8.2/moment.min',
-                      assetDetailCommits: protocol + 'v1commitstream.azurewebsites.net/assetDetailCommitsModule',
-                      handlebars: protocol + 'cdnjs.cloudflare.com/ajax/libs/handlebars.js/2.0.0-alpha.4/handlebars.amd.min'
+                        moment: protocol + 'cdnjs.cloudflare.com/ajax/libs/moment.js/2.8.2/moment.min',
+                        assetDetailCommits: protocol + 'v1commitstream.azurewebsites.net/assetDetailCommitsModule',
+                        handlebars: protocol + 'cdnjs.cloudflare.com/ajax/libs/handlebars.js/2.0.0-alpha.4/handlebars.amd.min'
                     },
                     config: {
                         moment: {
@@ -17,14 +17,14 @@
                         }
                     }
                 });
-            	require(['assetDetailCommits'], function(assetDetailCommits) {
+                require(['assetDetailCommits'], function(assetDetailCommits) {
                     assetDetailCommits(commitStreamDomId, assetNumber);
                 });
             });
         };
     } else {
         window.CommitStream.commitsDisplay = function(commitStreamDomId, assetNumber) {
-        	require(['assetDetailCommits'], function(assetDetailCommits) {
+            require(['assetDetailCommits'], function(assetDetailCommits) {
                 assetDetailCommits(commitStreamDomId, assetNumber);
             });
         }
