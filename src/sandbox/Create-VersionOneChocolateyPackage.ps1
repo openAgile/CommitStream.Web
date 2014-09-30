f759e7499687e47057d03efee42259fda3d87f03
Josh Gough <jsgough@gmail.com>
2014-09-30 11:45:50 -0400

    S-48322 ChocolateyPackage script

---
+++ src/sandbox/Create-VersionOneChocolateyPackage.ps1
@@ -0,0 +1,15 @@
+rm -re -fo VersionOne.ChocolateyPackage
+git clone git@github.com:versionone/VersionOne.ChocolateyPackage.git --branch S-47665_FeaturePackage --single-branch
+
+$version=gc VERSION
+
+$fileName="VersionOne.Setup-Ultimate-$version.cs.exe"
+echo "Uploading $fileName to S3"
+Write-S3Object -BucketName versionone-chocolatey -File $fileName -Key $fileName -PublicReadOnly
+
+cd VersionOne.ChocolateyPackage
+
+$url="https://s3.amazonaws.com/versionone-chocolatey/VersionOne.Setup-Ultimate-#version#.cs.exe"
+$feature="CommitStreamVersionOne"
+
+.\build.ps1 $version $url $feature
