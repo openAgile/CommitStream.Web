ed3d4dca4dfebad2250777433692203f9b5cbbde
Josh Gough <jsgough@gmail.com>
2014-10-02 08:20:35 -0400

    S-48324 Doc comments in queryController for /api/query

--- src/app/api/queryController.js
+++ src/app/api/queryController.js
@@ -24,6 +24,23 @@
 	}
 
 	controller.init = function (app) {
+		/**
+		 * @api {get} /api/query Request commits
+		 * @apiName query
+		 * @apiGroup Query
+		 *
+		 * @apiParam {workitem} VersionOne workitem identifier
+		 *
+		 * @apiSuccess {String} firstname Firstname of the User.
+		 * @apiSuccess {String} lastname  Lastname of the User.
+		 * @apiSuccess {String}	commitDate Original commit date
+		 * @apiSuccess {String} timeFormatted Time the commit was made, relative to now
+		 * @apiSuccess {String} author Commit author's name
+		 * @apiSuccess {String}	sha1Partial First 6 characters of the commit's SHA1 hash
+		 * @apiSuccess {String} action The original action that produced the commit, such as "commited"
+		 * @apiSuccess {String} message Original commit message sent to the VCS
+		 * @apiSuccess {String} commitHref Link to an HTML page to view the commit in the source VCS
+		 */		
 		app.get("/api/query", function (req, res) {
 			var options = {
 				host: config.eventStoreHost,
