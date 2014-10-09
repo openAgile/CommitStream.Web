e55d7641758d8568272a771e12dce1b4f368840d
Josh Gough <jsgough@gmail.com>
2014-10-09 15:05:24 -0400

    S-48324 Make settings use protocol and host/port instead of hardcoding it

--- src/app/api/settingsController.js
+++ src/app/api/settingsController.js
@@ -6,24 +6,31 @@
 
     var db = new JsonDB('instances', true, true);        
 
-    var defaultSettings = {
-        'CommitStream.Availability': 'unavailable',
-        'CommitStream.Toggle': 'off',
-        'CommitStream.AppUrl': 'http://v1commitstream-staging.azurewebsites.net/app.js'
-    };
+    function getDefaultSettings() {
+        return {
+            'CommitStream.Availability': 'unavailable',
+            'CommitStream.Toggle': 'off',
+            'CommitStream.AppUrl': 'http://localhost:6565/app'
+        };
+    }
 
     settingsController.init = function (app) {
 
         app.get('/api/settings', function(req, res) {
+            var settings = getDefaultSettings();
+            var protocol = req.protocol;
+            var host = req.get('host');
+            settings['CommitStream.AppUrl'] = protocol + '://' + host + '/app';
+
             var instance = req.query.instance;
             if (!instance) {
-                res.end(defaultSettings);
+               res.end(JSON.stringify(settings));
             }
             try {
                 var settings = db.getData('/' + instance);
                 res.end(JSON.stringify(settings));
             } catch (ex) {
-                res.end(JSON.stringify(defaultSettings));
+                res.end(JSON.stringify(settings));
             }
         });
         
@@ -38,7 +45,7 @@
                 settings = db.getData('/' + instance);
                 exists = true;
             } catch(ex) {
-                settings = JSON.parse(JSON.stringify(defaultSettings));
+                settings = getDefaultSettings();
             }
             for(var prop in req.body) {
                 settings[prop] = req.body[prop];
@@ -48,7 +55,7 @@
             } else {
                 db.push('/' + instance, settings);
             }
-            res.end("saved");
+            res.end('saved');
         });
     };
 })(module.exports);
