fbcd414214d707f610c2e7686c83870d631c230a
kunzimariano <kunzi.mariano@gmail.com>
2014-10-17 12:17:57 -0300

    S-48712 changed queryController.tests so it uses supertest.

--- src/app/api/test/queryController.tests.js
+++ src/app/api/test/queryController.tests.js
@@ -1,42 +1,30 @@
 // test/queryController.tests.js
-
 var assert = require("assert"),
     express = require('express'),
     app = express(),
-    request = require('request-json'),
+    request = require('supertest'),
     controller = require('../queryController');
 
-
-var port = 6667;
-var baseUrl = "http://localhost:" + port;
-var workItem = '123';
+controller.init(app);
 
 describe('queryController', function () {
-    before(function (done) {
-        controller.init(app);
-        app.listen(port, function () {
-            //console.log('CommitStream Web Server listening on port ' + port);
-            done();
-
-        });
-    });
-    
-    
     describe('When I issue a workitem query for an asset that has no associated commits', function () {
         var nock = require('nock');
+        //mock requests to event store
         var scope = nock('http://localhost:2113')
         .get('/streams/asset-123/head/backward/5?embed=content')
         .reply(404);
         
-        
         it('returns a 200 OK response with an empty commits array', function (done) {
-            var client = request.newClient(baseUrl);
-            client.get("/api/query?workitem=123", function (e, r, body) {
-                assert.equal(r.statusCode, 200);
-                assert.deepEqual(body.commits, []);
+            //exercise our api
+            request(app)
+            .get("/api/query?workitem=123")
+            .end(function (err, res) {
+                assert.equal(err, null);
+                assert.equal(res.statusCode, 200);
+                assert.deepEqual(res.body.commits, []);
                 done();
             });
         });
     });
-    
 });
