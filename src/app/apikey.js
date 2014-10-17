87e0ec21fcfc3b3fffeda3e52ade23fb4d564ed0
Josh Gough <jsgough@gmail.com>
2014-10-17 09:08:44 -0400

    S-48823 Bonehead mistake on missing != for string compare

--- src/app/apikey.js
+++ src/app/apikey.js
@@ -2,7 +2,7 @@
 	var config = require('./config');
 
 	module.exports = function (req, res, next) {
-    	if (!config.apiKey || (!req.query.key || !req.query.key == config.apiKey)) {
+    	if (!config.apiKey || !req.query.key || req.query.key != config.apiKey) {
         	res.status(401).send('API key parameter missing or invalid');
     	} else {
         	return next();
