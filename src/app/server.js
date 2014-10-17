8a963e37fe360ca56e90afbd916c2a28880293f8
Josh Gough <jsgough@gmail.com>
2014-10-16 22:15:29 -0400

    S-48823 WIP: created apikey.js middleware to encapsulate logic
    app.use(apikey);
    
    This makes each request go through this middleware. However, I don't use it
    until after setting up the static routes, which means they are still
    accessible without the API key. However, this makes /instances crippled
    because it wants an API key to call upon /api/settings now! So, this needs
    to be addressed or deferred cleanly.

--- src/app/server.js
+++ src/app/server.js
@@ -2,6 +2,7 @@ var express = require('express'),
     app = express(),
     cors = require('cors'),
     config = require('./config'),
+    apikey = require('./apikey'),
     exphbs = require('express-handlebars');
 
 app.get('/version', function(req, res) {
@@ -15,28 +16,25 @@ require('./bootstrapper').boot(config);
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 
+app.use(express.static(__dirname + '/client'));
+
+app.get('/instances', function(req, res) {
+    res.render('instances');
+});
 
 app.use(cors());
+app.use(apikey);
 
-// Map API the routes
-api.init(app);
 
 app.use(function(req, res, next) {
     res.setHeader("X-CommitStream-API-Docs", "https://github.com/eventstore/eventstore/wiki");
     return next();
 });
 
-
-function assertApiKeyValid(req, res) {
-    if (!config.apiKey || (!req.query.key || !req.query.key == config.apiKey)) {
-        res.status(403).send('API key parameter missing or invalid');
-        return false;
-    }
-    return true;
-}
+// Map API the routes
+api.init(app);
 
 app.get('/app', function(req, res) {
-    if (!assertApiKeyValid(req, res)) return;
     res.setHeader('content-type', 'application/javascript');
     var protocol = req.protocol;
     var host = req.get('host');
@@ -47,12 +45,6 @@ app.get('/app', function(req, res) {
         resourcePath: protocol + '://' + host + '/'
     });
 });
-
-app.get('/instances', function(req, res) {
-    res.render('instances');
-});
-
-app.use(express.static(__dirname + '/client'));
 
 app.listen(config.port, function () {
     console.log('CommitStream Web Server listening on port ' + config.port);
