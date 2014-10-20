cc35b158caa963330d883190109c3f33d54ebdf2
kunzimariano <kunzi.mariano@gmail.com>
2014-10-20 18:50:31 -0300

    S-48712 moved some of the projections logic into eventStore.js

--- src/app/api/helpers/eventStore.js
+++ src/app/api/helpers/eventStore.js
@@ -4,71 +4,98 @@ var request = require('request');
 var reqJson = require('request-json');
 
 function eventStore(baseUrl, userName, password) {
-    this.baseUrl = baseUrl;
-    this.username = userName;
-    this.password = password;
-    
-    this.authorization = 'Basic ' + new Buffer(this.username + ':' + this.password).toString('base64');
+  this.baseUrl = baseUrl;
+  this.username = userName;
+  this.password = password;
+  
+  this.authorization = 'Basic ' + new Buffer(this.username + ':' + this.password).toString('base64');
 }
 
 //TODO: add callback
 eventStore.prototype.pushEvents = function (events) {
-    var eventStoreUrl = this.baseUrl + '/streams/github-events';
-    
-    var options = {
-        url: eventStoreUrl,
-        body: events,
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': "application/vnd.eventstore.events+json",
-            'Content-Length': events.length,
-            'Authorization': this.authorization
-        }
-    };
-    
-    request.post(options, function (error, response, body) {
-        if (error) {
-            console.log(error);
-        } else {
-            console.log('Posted to eventstore.');
-            console.log(response.statusCode);
-        }
-    });
+  var eventStoreUrl = this.baseUrl + '/streams/github-events';
+  
+  var options = {
+    url: eventStoreUrl,
+    body: events,
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': "application/vnd.eventstore.events+json",
+      'Content-Length': events.length,
+      'Authorization': this.authorization
+    }
+  };
+  
+  request.post(options, function (error, response, body) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log('Posted to eventstore.');
+      console.log(response.statusCode);
+    }
+  });
         
 };
 
 eventStore.prototype.getLastCommit = function (owner, repo, callback) {
-    var eventStoreUrl = this.baseUrl + '/streams/repo-' + owner + '-' + repo + '/head?embed=content';
-    
-    var options = {
-        url: eventStoreUrl,            
-        headers: {
-            'Accept': 'application/json'
-        }
-    };
-    request.get(options, function (error, response, body) {
-        console.log('Getting the last commit for this repository');
-        if (response.statusCode == 404) {
-            callback('Stream not found, You need to do a full import', null);
-        }
-        if (response.statusCode == 200) {
-            callback(null, body);
-        }
-    });
+  var eventStoreUrl = this.baseUrl + '/streams/repo-' + owner + '-' + repo + '/head?embed=content';
+  
+  var options = {
+    url: eventStoreUrl,            
+    headers: {
+      'Accept': 'application/json'
+    }
+  };
+  request.get(options, function (error, response, body) {
+    console.log('Getting the last commit for this repository');
+    if (response.statusCode == 404) {
+      callback('Stream not found, You need to do a full import', null);
+    }
+    if (response.statusCode == 200) {
+      callback(null, body);
+    }
+  });
 };
 
 eventStore.prototype.getLastAssets = function (options, callback) {
-    
-    var path = '/streams/asset-' + 
+  
+  var path = '/streams/asset-' + 
     options.workitem +
     '/head/backward/' +
     options.pageSize +
     '?embed=content';
-    
-    var client = reqJson.newClient(this.baseUrl);
-    client.get(path, function (err, response, body) {
-        callback(err, body.entries);
-    });
+  
+  var client = reqJson.newClient(this.baseUrl);
+  client.get(path, function (err, response, body) {
+    callback(err, body.entries);
+  });
+
+};
+
+eventStore.prototype.createProjection = function (name, script) {
+  var eventStoreUrl = this.baseUrl + '/projections/continuous?emit=yes&checkpoints=yes&enabled=yes&name=' + name;
+  
+  var options = {
+    url: eventStoreUrl,            
+    headers: {
+      'Accept': 'application/json',
+      'Authorization': this.authorization,
+      'Content-Type': 'application/json;charset=utf-8',
+      'Content-Length': script.length
+    },
+    body:script
+  };
+
+  request.post(options, function (err, response, body) {
+    if (err) {
+      console.error('ERROR could not create projection ' + name + ':');
+      console.error(err);
+    }
+    else {
+      console.log('OK created projection ' + name);
+      console.log(body);
+    }
+  });
 
 };
 
--- src/app/bootstrapper.js
+++ src/app/bootstrapper.js
@@ -1,60 +1,38 @@
-(function(bootstrapper) {
-  bootstrapper.boot = function(config) {
+(function (bootstrapper) {
+  bootstrapper.boot = function (config) {
     var _ = require('underscore'),
-        request = require('request');
-
-    var options = {
-      url: config.eventStoreBaseUrl,
-      path: '/projections/all-non-transient'
-    };
+        request = require('request'),
+        //TODO: get out of here!
+        rjson = require('request-json'),
+        eventStore = require('./api/helpers/eventStore');
     
-    request.get(options, function(err, response) {
-      createProjections(response.projections);
+    //TODO: do this in eventStore.js
+    var client = rjson.newClient(config.eventStoreBaseUrl);
+    client.get('/projections/all-non-transient', function (err, response, body) {
+      createProjections(body.projections);
     });
-
+    
     function createProjections(projectionsFound) {
       var fs = require('fs');
       var dir = './projections/';
-
+      
+      var es = new eventStore(config.eventStoreBaseUrl, config.eventStoreUser, config.eventStorePassword);
+      
       console.log('Looking for projections...');
-      fs.readdir(dir,function(err,files){
+      fs.readdir(dir, function (err, files) {
         if (err) throw err;
-        files.forEach(function(name){
-          fs.readFile(dir + name,'utf-8',function(err, script){
+        files.forEach(function (name) {
+          fs.readFile(dir + name, 'utf-8', function (err, script) {
             if (err) throw err;
             else {
-              if (!_.findWhere(projectionsFound, {effectiveName:name})) {
-                createProjection(name, script);
+              if (!_.findWhere(projectionsFound, { effectiveName: name })) {
+                es.createProjection(name, script);
               } else {
                 console.log('OK found ' + name);
               }
             }
           });
         });
-      });
-    }
-    
-    //TODO: move this into eventStore.js
-    function createProjection(name, script) {
-      var options = {
-        url: 'http://admin:changeit@' + config.eventStoreHost + ':' 
-          + config.eventStorePort + '/projections/continuous?emit=yes&checkpoints=yes&enabled=yes&name=' + name,
-        headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json;charset=utf-8',
-          'Content-Length': script.length
-        },
-        body: script
-      };
-      request.post(options, function(err, response, body) {
-        if (err) { 
-          console.error('ERROR could not create projection ' + name + ':');
-          console.error(err);
-        }
-        else {
-          console.log('OK created projection ' + name);
-          console.log(body);
-        }
       });
     }
   }
