ac02617aa82806e089c52e833548d42d7583fd7e
Cory Wheeler <cory.wheeler@versionone.com>
2014-11-24 14:36:08 -0500

    Add some documentation to the digest endpoint.

--- src/app/api/digestController.js
+++ src/app/api/digestController.js
@@ -5,6 +5,20 @@
 
   digestController.init = function (app) {
 
+  /**
+     * The response to creating a digest will have links (see _links below) to other resources
+     * that are a result of having created a digest. Those links are identified for documentation
+     * purposes via their rel value.
+     *
+     * @api {post} /api/digest Create a new digest
+     * @apiName digest
+     *
+     * @apiSuccess {String} id - Id of the digest created.
+     * @apiSuccess {String} digestUrl - The url of the digest created.
+     * @apiSuccess {Array[Object]} _links - Links to other resources as a result of creating a digest.
+     *                               rel: 'inbox-form' links to a form for creating a new inbox for a repository.
+   **/
+
     app.post('/api/digest', function(req, res) {
       var response;
       var protocol = config.protocol || req.protocol;
