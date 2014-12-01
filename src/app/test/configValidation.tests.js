dc7c23357455f9d9db61aebb89cf2639e187830e
shawnmarie <shawn.abbott@versionone.com>
2014-12-01 14:14:55 -0500

    updated and added some config validation tests S-48999

--- src/app/test/configValidation.tests.js
+++ src/app/test/configValidation.tests.js
@@ -54,8 +54,18 @@ describe('configValidation', function() {
       done();
     });
 
-    it('should NOT raise an exception when the apiKey is equal to or more than 36 characters long.', function(done) {
+//updated by SMA
+    it('should NOT raise an exception when the apiKey is more than 36 characters long.', function(done) {
+      // 40 characters long
       configStub.apiKey = '0123456789012345678901234567890123456789';
+      expect(configValidation.validate).to.not.throw(Error);
+      done();
+    });
+
+//added by SMA
+    it('should NOT raise and exception when the apiKey is equal to 36 characters in length.', function(done)) {
+      // 36 characters long
+      configStub.apiKey = 'jklkifshe543890qwe345790lkjsh9123456';
       expect(configValidation.validate).to.not.throw(Error);
       done();
     });
@@ -84,28 +94,41 @@ describe('configValidation', function() {
       done();
     });
 
-    it('should NOT raise and exception when eventStorePassword is equal to or more than 36 characters long.', function(done) {
+//updated by SMA
+    it('should NOT raise an exception when eventStorePassword is equal to 36 characters long.', function(done) {
+      // 36 characters long
       configStub.eventStorePassword = '098765432109876543210987654321098765';
+      expect(configValidation.validate).to.not.throw(Error);
+      done();
+    });
+
+//added by SMA
+    it('should NOT raise an exception when the eventStorePassword is more than 36 characters in length.', function(done) {
+      // 39 characters long
+      configStub.eventStorePassword = 'iuytrewsdf5678902wdr432ju45klopw12scg@@';
       expect(configValidation.validate).to.not.throw(Error);
       done();
     });
 
   });
 
+//corrected by SMA -- it statement contained eventStorePassword rather than eventStoreUser
   describe('validateEventStoreUserIsSet', function() {
-    it('should raise an exception when eventStorePassword is not set.', function(done) {
+    it('should raise an exception when eventStoreUser is not set.', function(done) {
       configStub.eventStoreUser = undefined;
       expect(configValidation.validate).to.throw(Error);
       done();
     });
 
-    it('should raise an exception when eventStorePassword is an empty string.', function(done) {
+//corrected by SMA -- it statement contained eventStorePassword rather than eventStoreUser
+    it('should raise an exception when eventStoreUser is an empty string.', function(done) {
       configStub.eventStoreUser = '';
       expect(configValidation.validate).to.throw(Error);
       done();
     });
 
-    it('should NOT raise an exception when eventStorePassword has a value.', function(done) {
+//corrected by SMA -- it statement contained eventStorePassword rather than eventStoreUser
+    it('should NOT raise an exception when eventStoreUser has a value.', function(done) {
       configStub.eventStoreUser = 'admin';
       expect(configValidation.validate).to.not.throw(Error);
       done();
@@ -114,17 +137,24 @@ describe('configValidation', function() {
   });
 
 
+// SMA ???? here on down
+//comments added by SMA
   describe('validateEventStoreUri', function() {
     it('should raise an exception when eventStoreBaseUrl is not a valid URI.', function(done) {
       configStub.production = false;
+      //eventStoreBaseURL is undefined
       configStub.eventStoreBaseUrl = undefined;
       expect(configValidation.validate).to.throw(Error);
+      //eventStoreBaseURL is empty string
       configStub.eventStoreBaseUrl = '';
       expect(configValidation.validate).to.throw(Error);
+      //eventStoreBaseURL is not http or https
       configStub.eventStoreBaseUrl = 'www.localhost.com';
       expect(configValidation.validate).to.throw(Error);
+      //eventStoreBaseURL is not https or http
       configStub.eventStoreBaseUrl = 'httpc://localhost:2113';
       expect(configValidation.validate).to.throw(Error);
+      //eventStoreBaseURL too many /, but is https
       configStub.eventStoreBaseUrl = 'https:///localhost:2113';;
       expect(configValidation.validate).to.throw(Error);
       done();
@@ -146,11 +176,14 @@ describe('configValidation', function() {
   });
 
   describe('validateEventStoreHttpsUri', function() {
-    it('should raise an exception when eventStoreBaseUrl is not a valid https URI.', function(done) {
+    it('should raise an exception when in production mode and eventStoreBaseUrl is not a valid https URI.', function(done) {
       configStub.production = true;
       configStub.eventStoreBaseUrl = 'http://localhost:2113';
       expect(configValidation.validate).to.throw(Error);
       configStub.eventStoreBaseUrl = 'http://some.domain.net:2113';
+      expect(configValidation.validate).to.throw(Error);
+      //added by SMA
+      configStub.eventStoreBaseUrl = 'www.localhost.com:2113';
       expect(configValidation.validate).to.throw(Error);
       done();
     });
