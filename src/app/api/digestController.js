92fbd94848ce0e625c388913aa7468a465e10155
Cory Wheeler <cory.wheeler@versionone.com>
2014-12-03 10:42:46 -0500

    convert tests over to no longer be concerned with the http lifecycle, but rather with the construction of the hypermedia response specifically.
    S-47060

--- src/app/api/digestController.js
+++ src/app/api/digestController.js
@@ -3,6 +3,21 @@
   var uuid = require('uuid-v4'),
       config = require('../config');
 
+  digestController.constructHypermedia = function(protocol, host, digestId) {
+    return {
+      'id': digestId,
+      'digestUrl': protocol + '://' + host + '/api/digest/' + digestId,
+      '_links': [
+        {
+          'href' : protocol + '://' + host + '/api/digest/' + digestId + '/inbox/new',
+          'method': 'GET',
+          'description': 'Navigate to form for creating an inbox for a repository',
+          'rel': 'inbox-form'
+        }
+      ]
+    }
+  }
+
   digestController.init = function (app) {
 
   /**
--- src/app/test/api/digestController.tests.js
+++ src/app/test/api/digestController.tests.js
@@ -1,108 +1,62 @@
-var express = require('express'),
-  app = express(),
-  chai = require('chai'),
+var chai = require('chai'),
   should = chai.should(),
   controller = require('../../api/digestController'),
-  request = require('supertest'),
   validator = require('validator');
   _ = require('underscore');
 
 chai.config.includeStack = true;
 
-controller.init(app);
+describe('digestController', function() {
+  describe('when constructing a hypermedia response', function() {
+    var hypermedia = controller.constructHypermedia('http', 'localhost', '7f74aa58-74e0-11e4-b116-123b93f75cba');
 
-postDigest = function(shouldBehaveThusly) {
-  request(app)
-    .post('/api/digest', 'myfirstdigest')
-    .end(shouldBehaveThusly);
-};
-
-describe('digestController', function () {
-  describe('when creating a digest', function () {
-    it('digestUrl should be a valid URI', function(done) {
-      postDigest(function(err, res) {
-        validator.isURL(res.body.digestUrl).should.be.true;
-        done();
-      });
+    it('digestUrl should be a valid URI', function() {
+        validator.isURL(hypermedia.digestUrl).should.be.true;
     });
 
-    it('should have an id to identify the digest', function(done) {
-      postDigest(function(err, res) {
-        var id = res.body.id;
-        res.body.should.have.property('id');
-        done();
-      })
-    })
-
-    it('id for a digest should be a valid uuid', function(done) {
-      postDigest(function(err, res) {
-        var id = res.body.id;
-        validator.isUUID(id).should.be.true;
-        done();
-      })
-    })
-
-    it('digestUrl should contain the id of the digest', function(done) {
-      postDigest(function(err, res) {
-        var digestUrlParts = res.body.digestUrl.split('/');
-        var id = digestUrlParts[digestUrlParts.length - 1];
-        id.should.equal(res.body.id);
-        done();
-      });
+    it('should have an id to identify the digest', function() {
+        hypermedia.should.have.property('id');
     });
 
-    it('should have links to other resources', function(done) {
-      postDigest(function(err, res) {
-        res.body.should.include.key('_links');
-        done();
-      })
-    })
+    it('id for a digest should be a valid uuid', function() {
+      var id = hypermedia.id;
+      validator.isUUID(id).should.be.true;
+    });
 
-    it('should link to an inbox form - v1', function(done) {
-      postDigest(function(err, res) {
-        res.body.should.have.deep.property('_links[0].rel', 'inbox-form');
-        done();
-      })
-    })
+    it('digestUrl should contain the id of the digest', function() {
+      var digestUrlParts = hypermedia.digestUrl.split('/');
+      var id = digestUrlParts[digestUrlParts.length - 1];
+      id.should.equal(hypermedia.id);
+    });
 
-    it('should link to an inbox form - v2', function(done) {
-      postDigest(function(err, res) {
-        var link = _.find(res.body._links, function(element) { return element.rel === 'inbox-form'; });
-        link.should.have.property('rel', 'inbox-form');
-        done();
-      })
-    })
+    it('should have links to other resources', function() {
+      hypermedia.should.include.key('_links');
+    });
 
-    it('should have an HTTP GET verb to interract with the inbox form ', function(done) {
-      postDigest(function(err, res) {
-        var link = _.find(res.body._links, function(element) { return element.rel === 'inbox-form'; });
-        link.should.have.property('method', 'GET');
-        done();
-      })
-    })
+    it('should link to an inbox form', function() {
+      var link = _.find(hypermedia._links, function(element) { return element.rel === 'inbox-form'; });
+      link.should.have.property('rel', 'inbox-form');
+    });
 
-    it('should have a reference to the inbox form', function(done) {
-      postDigest(function(err, res) {
-        var response = res.body;
-        var link = _.find(response._links, function(element) { return element.rel === 'inbox-form'; });
-        link.should.have.property('href', response.digestUrl + '/inbox/new');
-        done();
-      })
-    })
+    it('should have an HTTP GET verb to interract with the inbox form ', function() {
+      var link = _.find(hypermedia._links, function(element) { return element.rel === 'inbox-form'; });
+      link.should.have.property('method', 'GET');
+    });
 
-    it('link for inbox form should have a description', function(done) {
-      postDigest(function(err, res) {
-        var response = res.body;
-        var link = _.find(response._links, function(element) { return element.rel === 'inbox-form'; });
-        link.should.have.property('description', 'Navigate to form for creating an inbox for a repository');
-        done();
-      })
-    })
+    it('should have a reference to the inbox form', function() {
+      var link = _.find(hypermedia._links, function(element) { return element.rel === 'inbox-form'; });
+      link.should.have.property('href', hypermedia.digestUrl + '/inbox/new');
+    });
 
-  });
-});
+    it('link for inbox form should have a description', function() {
+      var link = _.find(hypermedia._links, function(element) { return element.rel === 'inbox-form'; });
+      link.should.have.property('description', 'Navigate to form for creating an inbox for a repository');
+    });
 
+  })
+})
 
+// see jsonpath, appcatalog has some in it appcatalogentry.schema
 // {
 //   "digestUrl": "http://host/api/digest/1f1aa47629c44116a3ca08a9bb911309",
 //   "_links": [
@@ -113,4 +67,17 @@ describe('digestController', function () {
 //     "method": "GET"
 //     }
 //   ]
+// }
+
+
+// {
+//   'digest' : {
+//     'id' : '1f1aa47629c44116a3ca08a9bb911309',
+//     'href' : 'http://host/api/digest/',
+//     'links' : {
+//       'inbox' : {
+//         'href' : ''
+//       }
+//     }
+//   }
 // }
