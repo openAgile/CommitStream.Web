061557297fb7fc55dfeb1c9bdfaa83771ed6d944
Josh Gough <jsgough@gmail.com>
2014-12-12 17:09:14 -0500

    Support getEventsByType, fromCategory().when(eventSpec), and getStreamsInCategoryCount S-47060

--- src/app/test/projections/eventStoreEnvironment.js
+++ src/app/test/projections/eventStoreEnvironment.js
@@ -4,14 +4,44 @@ global.events = [];
 global.setEvents = function (events) {
     global.events = events;
 };
+
 global.getEvents = function () {
     return global.events;
 };
+
+global.getEventsByType = function(eventType) {
+    var events = [];
+    getEvents().forEach(function(evt) {
+        if (evt.eventType && evt.eventType === eventType) {
+            events.push(evt);
+        }
+    });
+    return events;
+};
+
 global.getStream = function (name) {
     return streamResult[name];
 };
+
+global.getStreamsInCategoryCount = function(category) {
+    var streamsInCategory = [];
+    for(var streamName in streamResult) {
+        if (streamName.indexOf(category) === 0) streamsInCategory.push(streamName);
+    }
+    return streamsInCategory.length;
+};
+
 global.fromStream = function (streamSource) {
     return {
+        when: function(eventSpec) {
+            for(var eventType in eventSpec) {
+                var callback = eventSpec[eventType];
+                var evts = getEventsByType(eventType);
+                evts.forEach(function(evt) {
+                    callback(null, evt);
+                });
+            }
+        },
         whenAny: function (callback) {
             getEvents().forEach(function (element) {
                 callback(null, element);
@@ -19,6 +49,7 @@ global.fromStream = function (streamSource) {
         }
     }
 };
+
 global.linkTo = function (name, value) {
     if (streamResult[name])
         streamResult[name].push(value);
@@ -26,6 +57,7 @@ global.linkTo = function (name, value) {
         streamResult[name] = [value];
     }
 };
+
 global.emit = function (name, type, value) {
     if (streamResult[name])
         streamResult[name].push(value);
@@ -33,4 +65,3 @@ global.emit = function (name, type, value) {
         streamResult[name] = [value];
     }
 };
-
