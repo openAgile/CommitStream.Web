5cfff4768ae83feeefc0a164b619e05095515e39
Josh Gough <jsgough@gmail.com>
2014-12-30 15:08:28 -0500

    Added script to set environment variables instead of needing to modify config.js S-49000

---
+++ src/app/smoke-test.vars.sh
@@ -0,0 +1,6 @@
+export production="false"
+export eventStoreBaseUrl="http://localhost:2113"
+export eventStorePassword="changeit"
+export eventStoreUser="admin"
+export eventStoreAllowSelfSignedCert="true"
+export apiKey="32527e4a-e5ac-46f5-9bad-2c9b7d607bd7"
