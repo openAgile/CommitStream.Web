1c3a547540c56cd9d75b8c1f3a3d42d11a357338
shawnmarie <shawn.abbott@versionone.com>
2015-01-05 15:01:40 -0500

    updated test-coverage.sh to output to tap file results.tap S-50749

--- src/app/test-coverage.sh
+++ src/app/test-coverage.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/sh
 export PATH=$(pwd)/node_modules/.bin:$PATH
 
-istanbul cover ./node_modules/mocha/bin/_mocha -- -u exports -R spec 'test/**/*tests.js'
+istanbul cover ./node_modules/mocha/bin/_mocha -- -u exports -R tap 'test/**/*tests.js' > results.tap
 istanbul report cobertura --root coverage
