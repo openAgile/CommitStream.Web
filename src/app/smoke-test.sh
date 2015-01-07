ca00a1c0edd01c6845d7e1a160cc966fcb29da40
Josh Gough <jsgough@gmail.com>
2015-01-07 11:23:48 -0500

    Set SCRIPT_HOME path to `pwd` for easy use both locally and in Jenkins
    S-50749

--- src/app/smoke-test.sh
+++ src/app/smoke-test.sh
@@ -1,8 +1,8 @@
-WORKSPACE=`pwd`
+SCRIPT_HOME=`pwd`
 cd /c/Program\ files/eventstore
 ./EventStore.ClusterNode.exe --mem-db --run-projections=all &
-sleep 20
-cd $WORKSPACE
+sleep 15
+cd $SCRIPT_HOME
 source ./smoke-test.vars.sh
 npm start &
 ./projections-enable.sh
