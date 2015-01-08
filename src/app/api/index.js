af2ecfadc3f8e8b832f8748e241c9af13802fb41
Josh Gough <jsgough@gmail.com>
2015-01-08 17:46:20 -0500

    Simplified index.js for importing controllers to reduce code. S-50918

--- src/app/api/index.js
+++ src/app/api/index.js
@@ -1,16 +1,15 @@
 (function (api) {
-
-	var importController = require('./importController'),
-		queryController = require('./queryController'),
-		settingsController = require('./settingsController'),
-		digestsController = require('./digestsController'),
-		inboxesController = require('./inboxesController');
-
 	api.init = function (app) {
-		importController.init(app);
-		queryController.init(app);
-		settingsController.init(app);
-		digestsController.init(app);
-		inboxesController.init(app);
+		var controllers = [
+			'import', 
+			'query', 
+			'settings',
+			'digests',
+			'inboxes'
+		];
+		controllers.forEach(function(controllerPrefix) {
+			var controller = require('./' + controllerPrefix + 'Controller');
+			controller.init(app);
+		});
 	};
 })(module.exports);
