23e037b9696d4ea4ee8381f55400cffe2182b540
Cory Wheeler <cory.wheeler@versionone.com>
2015-01-16 13:47:43 -0500

    Remove listenerWebhook as a resource.
    S-50918

--- src/app/api/importController.js
+++ src/app/api/importController.js
@@ -40,52 +40,6 @@
       res.end();
 
     });
-
-    app.post('/api/listenerWebhook', bodyParser.json(), function(req, res) {
-      res.set('Content-Type', 'application/json');
-      //TODO: all this logic, yikes!
-      if (!req.headers.hasOwnProperty('x-github-event')) {
-        res.json({
-          message: 'Unknown event type.'
-        });
-      } else if (req.headers['x-github-event'] == 'push') {
-
-        var translator = require('./translators/githubTranslator');
-        var events = translator.translatePush(req.body);
-        var es = new EventStore({
-          baseUrl: config.eventStoreBaseUrl,
-          username: config.eventStoreUser,
-          password: config.eventStorePassword
-        });
-        var e = JSON.stringify(events)
-        es.streams.post({
-          // name: 'github-events',
-          name: 'inboxCommits-85dacc66-a47b-4f6e-8ea0-6413c5324274',
-          events: e
-        }, function(error, response) {
-          if (error) {
-            console.log(error);
-          } else {
-            console.log('Posted to eventstore.');
-            console.log(response.statusCode);
-          }
-        });
-        res.json({
-          message: 'Your push event has been queued to be added to CommitStream.'
-        });
-
-      } else if (req.headers['x-github-event'] == 'ping') {
-        res.json({
-          message: 'Pong.'
-        });
-      } else {
-        res.json({
-          message: 'Unknown event type.'
-        });
-      }
-      res.end();
-    });
-
   };
 
 })(module.exports);
