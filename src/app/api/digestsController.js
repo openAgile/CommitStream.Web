8bf637b0f66bc5d59b6287854b884b8951f66f1e
Josh Gough <jsgough@gmail.com>
2015-04-09 17:08:32 -0400

    S-51508 WIP commit for modifying the happy path script to rely on internal systemEnableAll call for system projections.

--- src/app/api/digestsController.js
+++ src/app/api/digestsController.js
@@ -104,8 +104,8 @@
           partition: 'digest-' + req.params.uuid
         }, function(err, resp) {
           if (err) {
-            // throw err;
-            return res.sendGenericError();
+            throw err;
+            //return res.sendGenericError();
           } else if (resp && resp.statusCode === 408) {
             return res.sendGenericError('Trouble communicating with eventstore');
           } else if (!resp.body || resp.body.length < 1) {
