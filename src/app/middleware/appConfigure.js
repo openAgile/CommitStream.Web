b60103d94ab001eb69b082831afde064c9e551f3
Josh Gough <jsgough@gmail.com>
2015-04-10 13:11:28 -0400

    S-51508 Pull in resHal and resHref in appConfigure

--- src/app/middleware/appConfigure.js
+++ src/app/middleware/appConfigure.js
@@ -1,7 +1,12 @@
 (function() {
-	var sendGenericError = require('./sendGenericError');
+	var sendGenericError = require('./sendGenericError'),
+		href = require('./resHref'),
+		hal = require('./resHal');
+
 	module.exports = function(app) {
 		app.use(sendGenericError);
+		app.use(href);
+		app.use(hal);
 		return app;
 	};
 }())
