b03fe9f5d245d1c4e7bdc751a0b8de9a466ba3e0
MatiasHeffel <matiasheffel@hotmail.com>
2015-04-10 16:10:24 -0300

    S-51508 move queryStatePartitionById to ES helpers
    also move statusCodeValidator.js to the helpers folder

--- src/app/api/helpers/eventStoreClient.js
+++ src/app/api/helpers/eventStoreClient.js
@@ -1,9 +1,23 @@
 (function() {
   var EventStore = require('eventstore-client'),
-      config = require('../../config');
-  module.exports = new EventStore({
+    config = require('../../config'),
+    statusCodeValidator = require('./statusCodeValidator');
+
+  var client = new EventStore({
     baseUrl: config.eventStoreBaseUrl,
     username: config.eventStoreUser,
     password: config.eventStorePassword
   });
+
+  client.queryStatePartitionById = function(args) {
+    var stateArgs = {
+      name: args.name,
+      partition: args.name + '-' + args.id
+    };
+
+    return client.projection.getStateAsync(stateArgs)
+      .then(statusCodeValidator.validateGetProjection(args.name, args.id));
+  };
+
+  module.exports = client;
 })();
