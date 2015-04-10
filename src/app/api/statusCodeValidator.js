6df99d5ae94d21f88fe96f1e9536fe7944092f60
Josh Gough <jsgough@gmail.com>
2015-04-10 13:12:18 -0400

    S-51508 Validate status codes and raise exception for calls to eventstore-client

---
+++ src/app/api/statusCodeValidator.js
@@ -0,0 +1,24 @@
+(function(statusCodeValidator) {
+  var csError = require('../middleware/csError'),
+      Promise = require('bluebird');
+
+  var NotFound = csError.createCustomError('NotFound', function(message) {
+    message = message || 'Resource not found';
+    var errors = [message];
+    NotFound.prototype.constructor.call(this, errors, 404);
+    this.name = 'NotFound';
+  });  
+
+  statusCodeValidator.validateGetProjection = function(objectType, objectId) {
+    return function(response) {
+      if (!response.body || response.body.length < 1 || response.statusCode === 404) {
+        throw new NotFound('Could not find ' + objectType + ' with id ' + objectId + '.');
+      } else if (response.statusCode != 200) {
+        throw new Error(response.statusCode);
+      } else {
+          var data = JSON.parse(response.body);
+          return Promise.resolve(data);
+      }
+    };
+  };
+}(module.exports));
