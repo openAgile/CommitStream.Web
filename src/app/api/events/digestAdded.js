d38a53ebf80de46b3331e38b317eef4f01926970
kunzimariano <kunzi.mariano@gmail.com>
2015-04-13 16:30:50 -0300

    Refactored digestController so it makes use of instanceID, schema validation and the new event store helper. S-51678

--- src/app/api/events/digestAdded.js
+++ src/app/api/events/digestAdded.js
@@ -1,17 +1,40 @@
 (function(digestAdded) {
-	var uuid = require('uuid-v4');
+  var uuid = require('uuid-v4'),
+    schemaValidator = require('../schemaValidator');
 
-	digestAdded.create = function(description) {
-		var eventId = uuid();
-		var digestId = uuid();
+  digestAdded.create = function(instanceId, description) {
+    var eventId = uuid();
+    var digestId = uuid();
 
-		return {
-			eventType: 'DigestAdded',
-			eventId: eventId,
-			data: {
-				digestId: digestId,
-				description: description
-			}
-		};
-	};
+    return {
+      eventType: 'DigestAdded',
+      eventId: eventId,
+      data: {
+        instanceId: instanceId,
+        digestId: digestId,
+        description: description
+      }
+    };
+  };
+
+  digestAdded.SCHEMA = {
+    "title": "digest",
+    "type": "object",
+    "required": [
+      "description"
+    ],
+    "properties": {
+      "description": {
+        "title": "",
+        "type": "string",
+        "minLength": 1,
+        "maxLength": 140
+      }
+    }
+  };
+
+  digestAdded.validate = function(data) {
+    return schemaValidator.validate('digest', data, digestAdded.SCHEMA);
+  };
+
 })(module.exports);
