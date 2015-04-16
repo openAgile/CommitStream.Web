ad2041b47adbefb9e86dc485ed7d9668a77bde0a
Josh Gough <jsgough@gmail.com>
2015-04-16 16:55:01 -0400

    S-51508 required() instanceGet.js for instancesController

--- src/app/api/instances/instancesController.js
+++ src/app/api/instances/instancesController.js
@@ -1,18 +1,6 @@
 (function(instancesController) {
   instancesController.init = function(app) {
     app.post('/api/instances', require('./instanceCreate'));
-
-    app.get('/api/instances/:instanceId', function(req, res, next) {
-      if (!validator.isUUID(req.params.instanceId)) {
-        throw new Error("Must supply a valid instanceId. The value " + req.params.instanceId + ' is invalid.');
-      } else {
-        eventStore.queryStatePartitionById({
-          name: 'instance',
-          id: req.params.instanceId
-        }).then(function(instance) {
-          res.hal(instanceFormatAsHal(req.href, instance));
-        });
-      }
-    });
+    app.get('/api/instances/:instanceId', require('./instanceGet'));
   };
 }(module.exports));
