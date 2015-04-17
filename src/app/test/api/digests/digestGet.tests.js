fe0a8ee3bade2e64202654aeae481c369704cb56
Cory Wheeler <cory.wheeler@versionone.com>
2015-04-17 17:25:57 -0400

    S-51510:	WIP start to pull the test suite together for digestGet

--- src/app/test/api/digests/digestGet.tests.js
+++ src/app/test/api/digests/digestGet.tests.js
@@ -1,191 +1,223 @@
-/********************************************
+require('../handler-base')();
 
-   GET tests
+// Things that proxyquire will use.
 
-   *******************************************/
+var eventStore = {
+    queryStatePartitionById: sinon.stub()
+  },
+  digestFormatAsHal = sinon.stub(),
+  validator = {
+    isUUID: sinon.stub()
+  };
 
-describe('when requesting a digest', function() {
 
-  describe('with an invalid, non-uuid digest identifier', function() {
-    function get(shouldBehaveThusly) {
-      getDigest('/api/e9be4a71-f6ca-4f02-b431-d74489dee5d0/digests/not_a_uuid', shouldBehaveThusly);
-    }
+// Configure up the controller use proxyquire
+var handler = proxyquire('../../api/digests/digestGet', {
+  './digestFormatAsHal': digestFormatAsHal,
+  '../helpers/eventStoreClient': eventStore,
+  'validator': validator
+});
 
-    it('it returns a 400 status code', function(done) {
-      get(function(err, res) {
-        res.statusCode.should.equal(400);
-        done();
-      });
-    });
 
-    it('it returns a meaningful error message', function(done) {
-      get(function(err, res) {
-        res.text.should.equal('The value "not_a_uuid" is not recognized as a valid digest identifier.');
-        done();
-      });
-    });
-  });
 
-  describe('with a valid, uuid digest identifier', function() {
+describe('digestGet', function() {
 
-    var uuid = 'e9be4a71-f6ca-4f02-b431-d74489dee5d0';
-    var data = {
-      "description": "BalZac!",
-      "digestId": uuid
-    };
+  before(function() {
 
-    beforeEach(function() {
-      hypermediaResponseStub.digestGET = sinon.stub();
-      eventStoreClient.queryStatePartitionById = sinon.stub().resolves(data);
-    });
+  })
 
-    function get(shouldBehaveThusly) {
-      getDigest('/api/e9be4a71-f6ca-4f02-b431-d74489dee5d0/digests/' + uuid, shouldBehaveThusly);
-    }
+  it('it should run', function() {
+    console.log('-------------------------------- it runs')
 
-    it('calls eventStore.queryStatePartitionById with correct parameters', function(done) {
-      get(function(err, res) {
-        eventStoreClient.queryStatePartitionById.should.have.been.calledWith({
-          name: sinon.match.any,
-          id: uuid
-        });
-        done();
-      });
-    });
+  })
 
-    /*it('calls hypermediaResponse.digestGET with correct parameters', function(done) {
-        get(function(err, res) {
-          hypermediaResponseStub.digestGET.should.have.been.calledWith(
-            sinon.match.func, uuid, data
-          );
-          done();
-        });
-      });*/
 
-    it('it returns a 200 status code', function(done) {
-      get(function(err, res) {
-        res.statusCode.should.equal(200);
-        done();
-      });
-    });
+  //   describe('with an invalid, non-uuid digest identifier', function() {
+  //     function get(shouldBehaveThusly) {
+  //       getDigest('/api/e9be4a71-f6ca-4f02-b431-d74489dee5d0/digests/not_a_uuid', shouldBehaveThusly);
+  //     }
 
-    it('returns a Content-Type of application/hal+json', function(done) {
-      get(function(err, res) {
-        res.get('Content-Type').should.equal('application/hal+json; charset=utf-8');
-        done();
-      });
-    });
-  });
+  //     it('it returns a 400 status code', function(done) {
+  //       get(function(err, res) {
+  //         res.statusCode.should.equal(400);
+  //         done();
+  //       });
+  //     });
 
-  describe('with a valid, uuid that does not match a real digest', function() {
+  //     it('it returns a meaningful error message', function(done) {
+  //       get(function(err, res) {
+  //         res.text.should.equal('The value "not_a_uuid" is not recognized as a valid digest identifier.');
+  //         done();
+  //       });
+  //     });
+  //   });
 
-    beforeEach(function() {
-      eventStoreClient.queryStatePartitionById = sinon.stub().resolves();
-      eventStoreClient.streams.post = sinon.stub();
-      eventStoreClient.projection.getState.callsArgWith(1, null, {
-        body: ''
-      }); // No error, but nothing found on the remote end
-    });
+  //   describe('with a valid, uuid digest identifier', function() {
 
-    var uuid = 'ba9f6ac9-fe4a-4ddd-bf07-f1fb37be5dbf';
+  //     var uuid = 'e9be4a71-f6ca-4f02-b431-d74489dee5d0';
+  //     var data = {
+  //       "description": "BalZac!",
+  //       "digestId": uuid
+  //     };
 
-    function get(shouldBehaveThusly) {
-      getDigest('/api/ba9f6ac9-fe4a-4ddd-bf07-f1fb37be5dbf/digests/' + uuid, shouldBehaveThusly);
-    }
+  //     beforeEach(function() {
+  //       hypermediaResponseStub.digestGET = sinon.stub();
+  //       eventStoreClient.queryStatePartitionById = sinon.stub().resolves(data);
+  //     });
 
-    /* it('calls eventStore.projection.getState with correct parameters', function(done) {
-        get(function(err, res) {
-          eventStoreClient.projection.getState.should.have.been.calledWith({
-            name: sinon.match.any,
-            partition: 'digest-' + uuid
-          }, sinon.match.any);
-          done();
-        });
-      });
-      
-      it('it returns a 404 status code', function(done) {
-        get(function(err, res) {
-          res.statusCode.should.equal(404);
-          done();
-        });
-      });
+  //     function get(shouldBehaveThusly) {
+  //       getDigest('/api/e9be4a71-f6ca-4f02-b431-d74489dee5d0/digests/' + uuid, shouldBehaveThusly);
+  //     }
 
-      it('returns a Content-Type of application/json', function(done) {
-        get(function(err, res) {
-          res.get('Content-Type').should.equal('application/json; charset=utf-8');
-          done();
-        });
-      });
+  //     it('calls eventStore.queryStatePartitionById with correct parameters', function(done) {
+  //       get(function(err, res) {
+  //         eventStoreClient.queryStatePartitionById.should.have.been.calledWith({
+  //           name: sinon.match.any,
+  //           id: uuid
+  //         });
+  //         done();
+  //       });
+  //     });
 
-      it('it returns a meaningful error message', function(done) {
-        get(function(err, res) {
-          res.text.should.equal(JSON.stringify({
-            'error': 'Could not find a digest with id ' + uuid
-          }));
-          done();
-        });
-      });*/
-  });
+  // // MIGHT NOT BE NEEDED
+  //     /*it('calls hypermediaResponse.digestGET with correct parameters', function(done) {
+  //         get(function(err, res) {
+  //           hypermediaResponseStub.digestGET.should.have.been.calledWith(
+  //             sinon.match.func, uuid, data
+  //           );
+  //           done();
+  //         });
+  //       });*/
 
-  describe('with an error returned from eventStoreClient', function() {
+  //     it('it returns a 200 status code', function(done) {
+  //       get(function(err, res) {
+  //         res.statusCode.should.equal(200);
+  //         done();
+  //       });
+  //     });
 
-    beforeEach(function() {
-      eventStoreClient.projection.getState = sinon.stub();
-      eventStoreClient.streams.post = sinon.stub();
-      eventStoreClient.projection.getState.callsArgWith(1, 'Hey there is an error!', {});
-    });
+  //     it('returns a Content-Type of application/hal+json', function(done) {
+  //       get(function(err, res) {
+  //         res.get('Content-Type').should.equal('application/hal+json; charset=utf-8');
+  //         done();
+  //       });
+  //     });
+  //   });
 
-    var uuid = '4cc217e4-0802-4f0f-8218-f8e5772aac5b';
+  //   describe('with a valid, uuid that does not match a real digest', function() {
 
-    function get(shouldBehaveThusly) {
-      getDigest('/api/4cc217e4-0802-4f0f-8218-f8e5772aac5b/digests/' + uuid, shouldBehaveThusly);
-    }
-    /*
-      it('calls eventStore.projection.getState with correct parameters', function(done) {
-        get(function(err, res) {
-          eventStoreClient.projection.getState.should.have.been.calledWith({
-            name: sinon.match.any,
-            partition: 'digest-' + uuid
-          }, sinon.match.any);
-          done();
-        });
-      });
+  //     beforeEach(function() {
+  //       eventStoreClient.queryStatePartitionById = sinon.stub().resolves();
+  //       eventStoreClient.streams.post = sinon.stub();
+  //       eventStoreClient.projection.getState.callsArgWith(1, null, {
+  //         body: ''
+  //       }); // No error, but nothing found on the remote end
+  //     });
 
-      it('it returns a 500 status code', function(done) {
-        get(function(err, res) {
-          res.statusCode.should.equal(500);
-          done();
-        });
-      }); */
-  });
+  //     var uuid = 'ba9f6ac9-fe4a-4ddd-bf07-f1fb37be5dbf';
 
-  describe('and there is an HTTP timeout of 408 (Request Timeout) that occurs when getting information from eventstore', function() {
-    var response;
-    var uuid = '4cc217e4-0802-4f0f-8218-f8e5772aac5b';
+  //     function get(shouldBehaveThusly) {
+  //       getDigest('/api/ba9f6ac9-fe4a-4ddd-bf07-f1fb37be5dbf/digests/' + uuid, shouldBehaveThusly);
+  //     }
 
-    before(function() {
-      eventStoreClient.projection.getState.callsArgWith(1, null, {
-        statusCode: 408
-      });
-    });
+  // // MIGHT NOT BE NEEDED
+  //      it('calls eventStore.projection.getState with correct parameters', function(done) {
+  //         get(function(err, res) {
+  //           eventStoreClient.projection.getState.should.have.been.calledWith({
+  //             name: sinon.match.any,
+  //             partition: 'digest-' + uuid
+  //           }, sinon.match.any);
+  //           done();
+  //         });
+  //       });
 
-    function get(shouldBehaveThusly) {
-      getDigest('/api/4cc217e4-0802-4f0f-8218-f8e5772aac5b/digests/' + uuid, shouldBehaveThusly);
-    }
+  //       it('it returns a 404 status code', function(done) {
+  //         get(function(err, res) {
+  //           res.statusCode.should.equal(404);
+  //           done();
+  //         });
+  //       });
 
-    /*it('it should report that there was an internal error', function(done) {
-        get(function(error, response) {
-          shouldBeGenericError(response);
-          done();
-        });
+  //       it('returns a Content-Type of application/json', function(done) {
+  //         get(function(err, res) {
+  //           res.get('Content-Type').should.equal('application/json; charset=utf-8');
+  //           done();
+  //         });
+  //       });
 
-      });
+  //       it('it returns a meaningful error message', function(done) {
+  //         get(function(err, res) {
+  //           res.text.should.equal(JSON.stringify({
+  //             'error': 'Could not find a digest with id ' + uuid
+  //           }));
+  //           done();
+  //         });
+  //       });
+  //   });
 
-      it('it should report a status code of 500 (Internal Server Error)', function(done) {
-        get(function(error, response) {
-          response.status.should.equal(500);
-          done();
-        });
-      }); */
-  });
+  //   describe('with an error returned from eventStoreClient', function() {
+
+  //     beforeEach(function() {
+  //       eventStoreClient.projection.getState = sinon.stub();
+  //       eventStoreClient.streams.post = sinon.stub();
+  //       eventStoreClient.projection.getState.callsArgWith(1, 'Hey there is an error!', {});
+  //     });
+
+  //     var uuid = '4cc217e4-0802-4f0f-8218-f8e5772aac5b';
+
+  //     function get(shouldBehaveThusly) {
+  //         getDigest('/api/4cc217e4-0802-4f0f-8218-f8e5772aac5b/digests/' + uuid, shouldBehaveThusly);
+  //       }
+
+  //       // MIGHT NOT BE NEEDED
+  //       /*
+  //         it('calls eventStore.projection.getState with correct parameters', function(done) {
+  //           get(function(err, res) {
+  //             eventStoreClient.projection.getState.should.have.been.calledWith({
+  //               name: sinon.match.any,
+  //               partition: 'digest-' + uuid
+  //             }, sinon.match.any);
+  //             done();
+  //           });
+  //         });
+
+  //         it('it returns a 500 status code', function(done) {
+  //           get(function(err, res) {
+  //             res.statusCode.should.equal(500);
+  //             done();
+  //           });
+  //         }); */
+  //   });
+
+  //   describe('and there is an HTTP timeout of 408 (Request Timeout) that occurs when getting information from eventstore', function() {
+  //     var response;
+  //     var uuid = '4cc217e4-0802-4f0f-8218-f8e5772aac5b';
+
+  //     before(function() {
+  //       eventStoreClient.projection.getState.callsArgWith(1, null, {
+  //         statusCode: 408
+  //       });
+  //     });
+
+  //     function get(shouldBehaveThusly) {
+  //       getDigest('/api/4cc217e4-0802-4f0f-8218-f8e5772aac5b/digests/' + uuid, shouldBehaveThusly);
+  //     }
+
+
+  // // MIGHT NOT BE NEEDED
+  //     /*it('it should report that there was an internal error', function(done) {
+  //         get(function(error, response) {
+  //           shouldBeGenericError(response);
+  //           done();
+  //         });
+
+  //       });
+
+  //       it('it should report a status code of 500 (Internal Server Error)', function(done) {
+  //         get(function(error, response) {
+  //           response.status.should.equal(500);
+  //           done();
+  //         });
+  //       }); */
+  //   });
 });
