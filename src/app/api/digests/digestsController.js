caa058fbd68a7ef0549e0e345e4ea91c82542de1
Josh Gough <jsgough@gmail.com>
2015-04-20 15:37:33 -0400

    S-51678 WIP support for /api/:instanceId/digests to list all digests for an instance

--- src/app/api/digests/digestsController.js
+++ src/app/api/digests/digestsController.js
@@ -3,5 +3,6 @@
   digestsController.init = function(app) {
     app.post('/api/:instanceId/digests', bodyParser.json(), require('./digestCreate'));
     //app.get('/api/:instanceId/digests/:digestId', require('./digestGet'));
+    app.get('/api/:instanceId/digests', require('./digestsGet'));
   };
 }(module.exports));
