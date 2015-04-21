f548330d3f94af4ac6d5641e7a1a712cdc24ec83
kunzimariano <kunzi.mariano@gmail.com>
2015-04-21 13:14:18 -0300

    S-51679 Fixed a typo in the name of the controller.

--- src/app/api/inboxes/inboxesController.js
+++ src/app/api/inboxes/inboxesController.js
@@ -12,7 +12,7 @@
   inboxesController.init = function(app) {
     app.post('/api/:instanceId/digests/:digestId/inboxes', bodyParser.json(), require('./inboxCreate'));
 
-    app.post('/api/:instanceId/inboxes/:inboxId/commits', bodyParser.json(), require('./commitCreate'));
+    app.post('/api/:instanceId/inboxes/:inboxId/commits', bodyParser.json(), require('./commitsCreate'));
 
     app.get('/api/:instanceId/inboxes/:inboxId', require('./inboxGet'));
   };
