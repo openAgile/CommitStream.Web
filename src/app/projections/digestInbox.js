bf0c62f70c73c97f9035f81a7ea0c4727b85ecdc
Josh Gough <jsgough@gmail.com>
2015-04-22 17:00:52 -0400

    S-51679 Used fromCategory instead of fromStream for digestInbox projection

--- src/app/projections/digestInbox.js
+++ src/app/projections/digestInbox.js
@@ -2,7 +2,7 @@ var callback = function(state, ev) {
   linkTo('digestInbox-' + ev.data.digestId, ev);
 };
 
-fromStream('inboxes').when({
+fromCategory('inboxes').when({
   'InboxAdded': function(state, ev) {
     callback(state, ev);
   }
