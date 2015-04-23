36d88b1ed11f98cb10b35e33344030ba48032f7a
JogoShugh <jsgough>
2015-04-23 08:25:56 -0400

    S-51935 Added helpers/commitsGet.js as promise-returning common function for digestCommitsGet and instanceCommitsGet

---
+++ src/app/api/helpers/commitsGet.js
@@ -0,0 +1,35 @@
+(function() {
+  var config = require('../../config'),
+    _ = require('underscore'),
+    gitHubEventsToApiResponse = require('../translators/gitHubEventsToApiResponse'),
+    eventStore = require('../helpers/eventStoreClient'),
+    pager = require('../helpers/pager');
+
+  module.exports = function(query, stream, buildUri, cache) {
+    // TODO assert stream and buildUri
+
+    var pageSize = pager.getPageSize(query);
+    var currentPage = cache.get(query.page);
+
+    var args = {
+      name: stream,
+      count: pageSize,
+      pageUrl: currentPage,
+      embed: 'tryharder'
+    };
+
+    return eventStore.getFromStream(args)
+      .then(function(response) {        
+        var links = response.links;       
+        var apiResponse = gitHubEventsToApiResponse(response.entries);
+        var pagedResponse = pager.getPagedResponse(apiResponse, links, currentPage, buildUri, cache);
+        return pagedResponse;
+      }).catch(function(error) {
+        // TODO: not sure how clean this approach is, of totally ignoring ANY error. Maybe it should catch a specific error type...
+        return {
+          commits: [],
+          _links: {}
+        };
+      });
+  };
+}());
