19a3ef7124e89fd0fd5bc855d319cd86ce191def
kunzimariano <kunzi.mariano@gmail.com>
2015-05-14 16:48:59 -0300

    S-52553 Added instanceId and inboxId to response when deleting an inbox.

--- src/app/api/inboxes/inboxRemove.js
+++ src/app/api/inboxes/inboxRemove.js
@@ -1,24 +1,29 @@
-(function () {
-	var inboxRemoved = require('./inboxRemoved'),
-		eventStore = require('../helpers/eventStoreClient');
+(function() {
+  var inboxRemoved = require('./inboxRemoved'),
+    eventStore = require('../helpers/eventStoreClient');
 
-	module.exports = function (req, res) {
-		var instanceId = req.instance.instanceId,
-			inboxId = req.params.inboxId;
+  module.exports = function(req, res) {
+    var instanceId = req.instance.instanceId,
+      inboxId = req.params.inboxId;
 
-		eventStore.queryStatePartitionById({
-			name: 'inbox',
-			id: inboxId
-		}).then(function (inbox) {
-			var inboxRemovedEvent = inboxRemoved.create(instanceId, inbox.digestId, inboxId);
-			var args = {
-				name: 'inboxes-' + instanceId,
-				events: inboxRemovedEvent
-			};
-			eventStore.postToStream(args).then(function () {
-				res.status(200).json({ 'message': 'Your inbox has been succesfully removed.' });
-			});
-		});
-	};
+    eventStore.queryStatePartitionById({
+      name: 'inbox',
+      id: inboxId
+    }).then(function(inbox) {
+      var inboxRemovedEvent = inboxRemoved.create(instanceId, inbox.digestId, inboxId);
+      var args = {
+        name: 'inboxes-' + instanceId,
+        events: inboxRemovedEvent
+      };
+      eventStore.postToStream(args).then(function() {
+        var responseBody = {
+          instanceId: instanceId,
+          inboxId: inboxId,
+          message: 'Your inbox has been successfully removed.'
+        };
+        res.status(200).json(responseBody);
+      });
+    });
+  };
 
-} ());
+}());
