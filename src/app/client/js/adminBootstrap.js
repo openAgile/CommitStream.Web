c60f295612629c3aadfe8e75887e1f0b80114ded
Josh Gough <jsgough@gmail.com>
2015-05-19 12:16:34 -0400

    S-52318 Trying bootstrap-toggle approach

--- src/app/client/js/adminBootstrap.js
+++ src/app/client/js/adminBootstrap.js
@@ -1,7 +1,7 @@
 (function() { 
   function CommitStreamAdminLoad(serviceUrl, rootElementId) {
     var commitStreamAdminStyles = $('#' + rootElementId);
-    commitStreamAdminStyles.prepend($('<link type="text/css" rel="stylesheet" href="' + serviceUrl + '/bower_components/bootstrap-switch/dist/css/bootstrap3/bootstrap-switch.min.css">'));    
+    commitStreamAdminStyles.prepend($('<link type="text/css" rel="stylesheet" href="' + serviceUrl + '/bower_components/bootstrap-toggle/css/bootstrap-toggle.min.css">'));
     commitStreamAdminStyles.prepend($('<link type="text/css" rel="stylesheet" href="' + serviceUrl + '/bower_components/bootstrap/dist/css/bootstrap.css">'));
 
     var loadScripts = function(scripts) {
@@ -21,6 +21,15 @@
                 $get: function () { return serviceUrl; }
               }
             });
+            var scriptEl = $($('script[data-commitstream-root]')[0]);
+            console.log(scriptEl);
+            console.log(scriptEl.attr('data-commitstream-root'));
+            var baseUrl = '';
+            var src = scriptEl.attr('src');
+            console.log(src);
+            if (src.indexOf('/') !== 0) {
+              baseUrl = src.substr(0, src.indexOf('/js/adminBootstrap.js'));
+            }
             CommitStreamAdminBoot($('#' + rootElementId));
           }
         })
@@ -36,8 +45,7 @@
       serviceUrl + '/bower_components/angular-route/angular-route.min.js',
       serviceUrl + '/bower_components/angular-hal/angular-hal.js',
       serviceUrl + '/bower_components/rfc6570/rfc6570.js',
-      serviceUrl + '/bower_components/bootstrap-switch/dist/js/bootstrap-switch.min.js',
-      serviceUrl + '/bower_components/angular-bootstrap-switch/dist/angular-bootstrap-switch.min.js',
+      serviceUrl + '/bower_components/bootstrap-toggle/js/bootstrap-toggle.min.js',
       serviceUrl + '/js/admin.js'
     ];
 
