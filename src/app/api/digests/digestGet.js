b85a8e9adb8bf25600d7308970925d96cfcc8c75
MatiasHeffel <matiasheffel@hotmail.com>
2015-05-20 11:59:15 -0300

    S-52553 refactor DigestGet

--- src/app/api/digests/digestGet.js
+++ src/app/api/digests/digestGet.js
@@ -1,17 +1,17 @@
 (function() {
   var digestFormatAsHal = require('./digestFormatAsHal'),
-    eventStore = require('../helpers/eventStoreClient'),
     validateUUID = require('../validateUUID');
 
   module.exports = function(req, res, next) {
     validateUUID('digests', req.params.digestId);
 
-    eventStore.queryStatePartitionById({
-      name: 'digest',
-      id: req.params.digestId
-    }).then(function(digest) {
-      var hypermedia = digestFormatAsHal(req.href, req.params.instanceId, digest);
-      res.hal(hypermedia);
-    });
+    var digest = {
+      digestId: req.digest.digestId,
+      description: req.digest.description
+    }
+
+    var hypermedia = digestFormatAsHal(req.href, req.params.instanceId, digest);
+    res.hal(hypermedia);
+
   };
 }());
