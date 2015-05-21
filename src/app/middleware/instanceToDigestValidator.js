bcea30d9fb16fd5a1a2877e0c74c47e79626a5c1
MatiasHeffel <matiasheffel@hotmail.com>
2015-05-21 17:29:26 -0300

    S-52553
    Sent always the hole event from ES to the api

--- src/app/middleware/instanceToDigestValidator.js
+++ src/app/middleware/instanceToDigestValidator.js
@@ -13,8 +13,8 @@
       name: 'digest',
       id: digestId
     }).then(function(digest) {
-      if (req.instance.instanceId === digest.instanceId) {
-        req.digest = digest;
+      if (digest.eventType === 'DigestAdded' && req.instance.instanceId === digest.data.instanceId) {
+        req.digest = digest.data;
         next();
       } else {
         throw new InvalidInstanceToDigest(req.instance.instanceId, digestId);
