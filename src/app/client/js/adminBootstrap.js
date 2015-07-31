35a33cef1d4a72a5e491bbcd594258a6d3cac103
kunzimariano <kunzi.mariano@gmail.com>
2015-07-31 14:30:38 -0300

    D-09481 Fixed XDomain being loaded more than once.

--- src/app/client/js/adminBootstrap.js
+++ src/app/client/js/adminBootstrap.js
@@ -1,80 +1,89 @@
 (function() {
-  try {
-    'use strict';
+  'use strict';
 
-    var prependStyleSheet = function(el, href) {
-      el.prepend($('<link type="text/css" rel="stylesheet" href="' + href + '">'));
-    }
+  var prependStyleSheet = function(el, href) {
+    el.prepend($('<link type="text/css" rel="stylesheet" href="' + href + '">'));
+  }
 
-    var scriptEl = $($('script[data-commitstream-root]')[0]);
-    var serviceUrl = '';
+  var getServiceUrl = function(scriptEl) {
     var src = scriptEl.attr('src');
     if (src.indexOf('/') !== 0) {
-      serviceUrl = src.substr(0, src.indexOf('/js/adminBootstrap.js'));
+      return src.substr(0, src.indexOf('/js/adminBootstrap.js'));
     }
+    return '';
+  }
 
-    var rootElementId = scriptEl.attr('data-commitstream-root');
-    var commitStreamRoot = $('#' + rootElementId);
-    // TODO: enable after new styles are released in V1 Prod
-    //prependStyleSheet(commitStreamRoot, serviceUrl + '/css/manage.css');
-    prependStyleSheet(commitStreamRoot, serviceUrl + '/css/bootstrap-toggle.min.css');
-    //prependStyleSheet(commitStreamRoot, serviceUrl + '/css/bootstrap-theme.min.css');
-    prependStyleSheet(commitStreamRoot, serviceUrl + '/css/glyphicon.css');
+  var callAngular = function() {
+    var config = angular.module('commitStreamAdmin.config', []);
 
-    // XDomain support for IE9 especially
+    config.provider('serviceUrl', function() {
+      return {
+        $get: function() {
+          return serviceUrl;
+        }
+      }
+    });
+    config.provider('configGetUrl', function() {
+      return {
+        $get: function() {
+          return configGetUrl;
+        }
+      }
+    });
+    config.provider('configSaveUrl', function() {
+      return {
+        $get: function() {
+          return configSaveUrl;
+        }
+      }
+    });
+
+    CommitStreamAdminBoot($('#' + rootElementId));
+  }
+
+  var loadScripts = function(scripts) {
+    if (!scripts || !scripts.length) {
+      return;
+    }
+    var currentScript = scripts.shift();
+    $.getScript(currentScript)
+      .done(function() {
+        if (scripts.length > 0) {
+          loadScripts(scripts);
+        } else {
+          callAngular();
+        }
+      })
+      .fail(onLoadScriptsFailure);
+  };
+
+  var onLoadScriptsFailure = function(jqXhr, settings, exception) {
+    console.error('CommitStream dependency loading exception for script:' + currentScript);
+    console.error('CommitStream dependency loading exception details:');
+    console.error(exception);
+  }
+
+  var prependXdomain = function(commitStreamRoot) {
     setTimeout(function() {
-      commitStreamRoot.prepend($('<scr' + 'ipt src="' + serviceUrl + '/bower_components/xdomain/dist/xdomain.min.js" slave="' + serviceUrl + '/proxy.html"></scr' + 'ipt>"'));
+      if (window.xdomain) {
+        return;
+      }
+      commitStreamRoot.prepend(
+        $('<scr' + 'ipt src="' + serviceUrl + '/bower_components/xdomain/dist/xdomain.min.js" slave="' + serviceUrl + '/proxy.html"></scr' + 'ipt>"')
+      );
     }, 1000);
+  }
 
-    // Grab route locations for config-get and config-save, if they exist
+  try {
 
-    // TODO handle null case
+    var scriptEl = $($('script[data-commitstream-root]')[0]);
+
+    var serviceUrl = getServiceUrl(scriptEl);
     var configGetUrl = scriptEl.attr('data-commitstream-config-get-url');
     var configSaveUrl = scriptEl.attr('data-commitstream-config-save-url');
 
-    var loadScripts = function(scripts) {
-      if (!scripts || !scripts.length) {
-        return;
-      }
-      var currentScript = scripts.shift();
-      $.getScript(currentScript)
-        .done(function() {
-          if (scripts.length > 0) {
-            loadScripts(scripts);
-          } else {
-            // Bootstrap time...
-            var config = angular.module('commitStreamAdmin.config', []);
-            config.provider('serviceUrl', function() {
-              return {
-                $get: function() {
-                  return serviceUrl;
-                }
-              }
-            });
-            config.provider('configGetUrl', function() {
-              return {
-                $get: function() {
-                  return configGetUrl;
-                }
-              }
-            });
-            config.provider('configSaveUrl', function() {
-              return {
-                $get: function() {
-                  return configSaveUrl;
-                }
-              }
-            });
-
-            CommitStreamAdminBoot($('#' + rootElementId));
-          }
-        })
-        .fail(function(jqXhr, settings, exception) {
-          console.error('CommitStream dependency loading exception for script:' + currentScript);
-          console.error('CommitStream dependency loading exception details:');
-          console.error(exception);
-        });
-    };
+    var rootElementId = scriptEl.attr('data-commitstream-root');
+    var commitStreamRoot = $('#' + rootElementId);
 
     var scripts = [
       serviceUrl + '/bower_components/angular/angular.min.js',
@@ -90,6 +99,15 @@
       serviceUrl + '/js/controllers.js',
       serviceUrl + '/js/directives.js'
     ];
+
+    // TODO: enable after new styles are released in V1 Prod
+    //prependStyleSheet(commitStreamRoot, serviceUrl + '/css/manage.css');
+    prependStyleSheet(commitStreamRoot, serviceUrl + '/css/bootstrap-toggle.min.css');
+    //prependStyleSheet(commitStreamRoot, serviceUrl + '/css/bootstrap-theme.min.css');
+    prependStyleSheet(commitStreamRoot, serviceUrl + '/css/glyphicon.css');
+
+    // XDomain support for IE9 especially
+    prependXdomain(commitStreamRoot);
 
     loadScripts(scripts);
   } catch (ex) {
