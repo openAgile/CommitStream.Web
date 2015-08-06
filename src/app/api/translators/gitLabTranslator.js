e8a8a71fd29971269a29cb9655d4682da2c7b384
Cory Wheeler <corywheeler@gmail.com>
2015-08-06 11:31:05 -0400

    S-53159: Add in a build job to compile only the gitLabTranslator from es6 to es5, and put in a tiny shim to see it do some real work.
    this would be called a baby step.

---
+++ src/app/api/translators/gitLabTranslator.js
@@ -0,0 +1,33 @@
+// FORM/POST PARAMETERS
+// None
+// HEADERS
+
+// Content-Length: 954
+
+// Host: requestb.in
+
+// X-Gitlab-Event: Push Hook
+
+// Total-Route-Time: 0
+
+// X-Request-Id: ca31f1a6-b95b-4fb9-ad33-bd5b9cdb8be0
+
+// Connect-Time: 2
+
+// Content-Type: application/json
+
+// Connection: close
+
+// Via: 1.1 vegur
+
+// matiasheffel [5:01 PM]
+// the raw body
+
+// matiasheffel [5:01 PM]
+// {"object_kind":"push","before":"8fa95cebec33f63f8e080ffbd11f7b8bae61f9c6","after":"95071710195f2aefc4ba7a5a2fd7d119b4123482","ref":"refs/heads/master","checkout_sha":"95071710195f2aefc4ba7a5a2fd7d119b4123482","message":null,"user_id":1,"user_name":"Administrator","user_email":"user@example.com","project_id":1,"repository":{"name":"gitlab-project-test","url":"git@137.135.123.9:user/gitlab-project-test.git","description":"","homepage":"http://137.135.123.9/user/gitlab-project-test","git_http_url":"http://137.135.123.9/user/gitlab-project-test.git","git_ssh_url":"git@137.135.123.9:user/gitlab-project-test.git","visibility_level":20},"commits":[{"id":"95071710195f2aefc4ba7a5a2fd7d119b4123482","message":"S-0001 hello\n","timestamp":"2015-08-05T18:00:13-03:00","url":"http://137.135.123.9/user/gitlab-project-test/commit/95071710195f2aefc4ba7a5a2fd7d119b4123482","author":{"name":"Administrator","email":"user@example.com"}}],"total_commits_count":1}
+
+// http://requestb.in/1mazrmi1?inspect
+
+"use strict";
+
+(function () {})();
