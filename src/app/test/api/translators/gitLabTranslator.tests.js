93b36e2bee36fc03add3bebafcb75c59f55902cc
Cory Wheeler <corywheeler@gmail.com>
2015-08-10 12:27:29 -0400

    S-53159: the responsiblity of checking headers had ben extracted into it's own function which was independantly testable, so this is updating the tests to reflect that.
    it also adds some tests for the case where checking headers a success case,
    and that the check is actually made when discovering if it can be translated.

--- src/app/test/api/translators/gitLabTranslator.tests.js
+++ src/app/test/api/translators/gitLabTranslator.tests.js
@@ -5,32 +5,68 @@ var chai = require('chai'),
   gitLabTranslator = require('../../../api/translators/gitLabTranslator');
 
 describe('gitLabTranslator', function() {
-  it('should say it can not translate the request if x-gitlab-event header is missing', function() {
+  it('should report incorrect headers if x-gitlab-event header is missing', function() {
     var request = {
       'headers': {}
     }
 
-    gitLabTranslator.canTranslate(request).should.equal(false);
+    gitLabTranslator.hasCorrectHeaders(request.headers).should.equal(false);
   });
 
-  it('should say it can not translate the request if x-gitlab-event header is wrong', function() {
+  it('should report incorrect headers if  x-gitlab-event header is wrong', function() {
     var request = {
       'headers': {
         'x-gitlab-event': 'Wrong Header Value'
       }
     }
 
-    gitLabTranslator.canTranslate(request).should.equal(false);
+    gitLabTranslator.hasCorrectHeaders(request.headers).should.equal(false);
   });
 
-  it('should say it can translate the request if x-gitlab-event header is Push Hook', function() {
+  it('should report correct headers if x-gitlab-event header is Push Hook', function() {
     var request = {
       'headers': {
         'x-gitlab-event': 'Push Hook'
       }
     }
 
-    gitLabTranslator.canTranslate(request).should.equal(true);
+    gitLabTranslator.hasCorrectHeaders(request.headers).should.equal(true);
   });
 
-})
+  describe('with appropriate headers', function() {
+
+    var originalHasCorrectHeaders;
+    var request;
+
+    before(function() {
+      request = {
+        'headers': {
+          'x-gitlab-event': 'Push Hook'
+        }
+      };
+
+      // Set hasCorrectHeaders to be a spy to ensure it's been called.
+      originalHasCorrectHeaders = gitLabTranslator.hasCorrectHeaders;
+      gitLabTranslator.hasCorrectHeaders = sinon.stub();
+      gitLabTranslator.hasCorrectHeaders.returns(true);
+    });
+
+    it('canTranslate should check for appropriate headers', function() {
+      // Act, which should cause the call to hasCorrectHeaders
+      gitLabTranslator.canTranslate(request);
+
+      // Check that hasCorrectHeaders was called
+      gitLabTranslator.hasCorrectHeaders.should.have.been.calledOnce;
+    });
+
+    it('canTranslate should return true when valid headers are available', function() {
+      gitLabTranslator.canTranslate(request).should.equal(true);
+    });
+
+    after(function() {
+      // reset hasCorrectHeaders back to the real implementation, not our spy.
+      gitLabTranslator.hasCorrectHeaders = originalHasCorrectHeaders;
+    });
+  });
+
+});
