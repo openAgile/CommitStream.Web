1e5f49ba7f839da38ff90efbab22e98e4df55c89
matiasheffel <matiasheffel@hotmail.com>
2015-08-12 15:59:53 -0300

    S-54524 added grunt dev as a task

--- src/app/Gruntfile.js
+++ src/app/Gruntfile.js
@@ -18,6 +18,16 @@ module.exports = function(grunt) {
   };
 
   grunt.initConfig({
+    express: {
+      options: {
+        port: 6565
+      },
+      dev: {
+        options: {
+          script: 'server.js'
+        }
+      }
+    },
     babel: {
       es6: {
         files: [babelFiles]
@@ -26,12 +36,40 @@ module.exports = function(grunt) {
         optional: 'runtime'
       }
     },
+    less: {
+      options: {
+        paths: ['client/css']
+      },
+      // target name
+      src: {
+        // no need for files, the config below should work
+        expand: true,
+        cwd: "client/css",
+        src: "*.less",
+        dest: "client/css",
+        ext: ".css"
+      }
+    },
     watch: {
       babel: {
         files: es6Locations,
         tasks: ['babel'],
         options: {
           spawn: false
+        }
+      },
+      less: {
+        files: ['client/css/*.less'], // which files to watch
+        tasks: ['less'],
+        options: {
+          nospawn: true
+        }
+      },
+      express: {
+        files: ['api/**/*.js', 'middleware/**/*.js', 'server.js'],
+        tasks: ['express:dev'],
+        options: {
+          spawn: false // for grunt-contrib-watch v0.5.0+, "nospawn: true" for lower versions. Without this option specified express won't be reloaded 
         }
       }
     }
@@ -42,6 +80,8 @@ module.exports = function(grunt) {
     grunt.config('babel.es6.files', [babelFiles]);
   });
 
-  grunt.registerTask('default', ['babel']);
+  grunt.registerTask('dev', ['less', 'babel', 'express', 'watch']);
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-express-server');
 };
