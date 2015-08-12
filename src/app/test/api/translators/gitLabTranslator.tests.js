e433d01da4147bea9fca620a6836b443e3c9c6a1
Cory Wheeler <corywheeler@gmail.com>
2015-08-12 16:42:16 -0400

    S-53159: unit test of what we would expect for the output of the gitLab translator given an input.

--- src/app/test/api/translators/gitLabTranslator.tests.js
+++ src/app/test/api/translators/gitLabTranslator.tests.js
@@ -2,8 +2,42 @@ var chai = require('chai'),
   should = chai.should(),
   proxyquire = require('proxyquire'),
   sinon = require('sinon'),
-  gitLabTranslator = require('../../../api/translators/gitLabTranslator'),
-  GitLabCommitMalformedError = require('../../../middleware/gitLabCommitMalformedError');
+  GitLabCommitMalformedError = require('../../../middleware/gitLabCommitMalformedError'),
+  uuidStub = sinon.stub(),
+  gitLabTranslator = proxyquire('../../../api/translators/gitLabTranslator', {
+    'uuid-v4': uuidStub
+  });
+
+var pushEventWithOneCommit = {
+  "object_kind": "push",
+  "before": "95790bf891e76fee5e1747ab589903a6a1f80f22",
+  "after": "da1560886d4f094c3e6c9ef40349f7d38b5d27d7",
+  "ref": "refs/heads/master",
+  "user_id": 4,
+  "user_name": "John Smith",
+  "user_email": "john@example.com",
+  "project_id": 15,
+  "repository": {
+    "name": "Diaspora",
+    "url": "git@example.com:mike/diasporadiaspora.git",
+    "description": "",
+    "homepage": "http://example.com/mike/diaspora",
+    "git_http_url": "http://example.com/mike/diaspora.git",
+    "git_ssh_url": "git@example.com:mike/diaspora.git",
+    "visibility_level": 0
+  },
+  "commits": [{
+    "id": "b6568db1bc1dcd7f8b4d5a946b0b91f9dacd7327",
+    "message": "Update Catalan translation to e38cb41.",
+    "timestamp": "2011-12-12T14:27:31+02:00",
+    "url": "http://example.com/mike/diaspora/commit/b6568db1bc1dcd7f8b4d5a946b0b91f9dacd7327",
+    "author": {
+      "name": "Jordi Mallach",
+      "email": "jordi@softcatala.org"
+    }
+  }],
+  "total_commits_count": 4
+};
 
 describe('gitLabTranslator', function() {
 
@@ -20,7 +54,6 @@ describe('gitLabTranslator', function() {
 
     it('canTranslate should check for appropriate headers', function() {
       var request = {};
-
       // Act, which should cause the call to hasCorrectHeaders
       gitLabTranslator.canTranslate(request);
 
@@ -138,5 +171,60 @@ describe('gitLabTranslator', function() {
 
       invokeTranslatePush.should.throw(GitLabCommitMalformedError);
     })
+  });
+
+  describe('when translating a push event that contains one commit', function() {
+    it('should match the expected translation', function() {
+      var instanceId = '73b40eab-bbb9-4478-9031-601b9e701d17',
+        digestId = '9c369aef-b041-4a38-a76c-d3cf59dec0d2',
+        inboxId = '9c369aef-b041-4a38-a76c-d3cf59dec0d2';
+      eventId = '87b66de8-8307-4e03-b2d3-da447c66501a';
+      uuidStub.returns(eventId);
+
+      var expected = [{
+        eventId: eventId,
+        eventType: 'GitLabCommitReceived',
+        data: {
+          sha: 'b6568db1bc1dcd7f8b4d5a946b0b91f9dacd7327',
+          commit: {
+            author: {
+              email: "jordi@softcatala.org",
+              name: "Jordi Mallach"
+            },
+            committer: {
+              date: "2011-12-12T14:27:31+02:00",
+              email: "jordi@softcatala.org",
+              name: "Jordi Mallach"
+            },
+            message: "Update Catalan translation to e38cb41."
+          },
+          html_url: 'http://example.com/mike/diaspora/commit/b6568db1bc1dcd7f8b4d5a946b0b91f9dacd7327',
+          repository: {
+            name: "Diaspora"
+          },
+          branch: 'master',
+          originalMessage: {
+            author: {
+              email: "jordi@softcatala.org",
+              name: "Jordi Mallach"
+            },
+            id: "b6568db1bc1dcd7f8b4d5a946b0b91f9dacd7327",
+            message: "Update Catalan translation to e38cb41.",
+            timestamp: "2011-12-12T14:27:31+02:00",
+            url: "http://example.com/mike/diaspora/commit/b6568db1bc1dcd7f8b4d5a946b0b91f9dacd7327"
+          }
+        },
+        metadata: {
+          instanceId: '73b40eab-bbb9-4478-9031-601b9e701d17',
+          digestId: '9c369aef-b041-4a38-a76c-d3cf59dec0d2',
+          inboxId: '9c369aef-b041-4a38-a76c-d3cf59dec0d2'
+        }
+      }];
+
+      var actual = gitLabTranslator.translatePush(pushEventWithOneCommit, instanceId, digestId, inboxId);
+
+      actual.should.deep.equal(expected);
+    });
+
   });
 });
