34824a7e28b4a72e3658b6254b426a06256a9907
kunzimariano <kunzi.mariano@gmail.com>
2015-08-21 10:11:13 -0300

    S-53161 added translatePush function to bitbucketTranslator.

--- src/app/api/translators/bitbucketTranslator.js
+++ src/app/api/translators/bitbucketTranslator.js
@@ -15,4 +15,56 @@
     }
     return false;
   };
+
+  bitbucketTranslator.translatePush = function (pushEvent, instanceId, digestId, inboxId) {
+
+    try {
+      var _ret = (function () {
+        var branch = pushEvent.push.changes[0]['new'].name;
+        var repository = {
+          id: pushEvent.repository.uuid,
+          name: pushEvent.repository.name
+        };
+        var events = _.map(pushEvent.push.changes[0].commits, function (aCommit) {
+          var commit = {
+            sha: aCommit.hash,
+            commit: {
+              author: aCommit.author.user.username,
+              // bitbucket does not have a commit.committer object. Using the same thing as author for now.
+              committer: {
+                name: aCommit.author.user.display_name,
+                email: aCommit.author.raw,
+                date: ''
+              },
+              message: aCommit.message
+            },
+            html_url: aCommit.links.html.href,
+            repository: repository,
+            branch: branch,
+            originalMessage: aCommit
+          };
+          return {
+            eventId: uuid(),
+            eventType: 'BitbucketCommitReceived',
+            data: commit,
+            metadata: {
+              instanceId: instanceId,
+              digestId: digestId,
+              inboxId: inboxId
+            }
+          };
+        });
+
+        return {
+          v: events
+        };
+      })();
+
+      if (typeof _ret === 'object') return _ret.v;
+    } catch (ex) {
+      console.log(ex);
+      throw ex;
+      //throw new BitbucketCommitMalformedError(ex, pushEvent);
+    }
+  };
 })(module.exports);
