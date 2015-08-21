0824183a78c46ba12b23bc804dd0df9ca02fad01
kunzimariano <kunzi.mariano@gmail.com>
2015-08-21 17:15:47 -0300

    S-53161 a not so cool way of dealing with the different families in commitEventsToApiResponse.js

--- src/app/api/translators/commitEventsToApiResponse.js
+++ src/app/api/translators/commitEventsToApiResponse.js
@@ -1,9 +1,19 @@
+//TODO: convert to ES6
 (function() {
   var _ = require('underscore'),
     moment = require('moment');
 
-  var getRepoInfo = function(commitUrl) {
-    var repoArray = commitUrl.split('/commit')[0].split('/');
+  var getRepoInfo = function(family, commitUrl) {
+
+    var repoArray;
+
+    if (family === 'GitHub' || family === 'GitLab') {
+      repoArray = commitUrl.split('/commit')[0].split('/');
+    } else if (family === 'Bitbucket') {
+      repoArray = commitUrl.split('/commits')[0].split('/');
+    } else {
+      throw 'Invalid family';
+    }
 
     var r = {};
     r.repoName = repoArray.pop();
@@ -26,16 +36,25 @@
     return repoInfo.serverUrl + '/' + repoInfo.repoOwner + '/' + repoInfo.repoName;
   }
 
-  var getBranchHref = function(repoHref, branch) {
-    // http://serverUrl/repoOwner/reponame/tree/branch
-    return repoHref + '/tree/' + branch;
+  var getBranchHref = function(family, repoHref, branch) {
+    if (family === 'GitHub' || family || 'GitLab') {
+      // http://serverUrl/repoOwner/reponame/tree/branchName
+      return repoHref + '/tree/' + branch;
+    }
+
+    if (family === 'bitbucket') {
+      // https://bitbucket.org/kunzimariano/test/branch/master for bitbucket
+      return repoHref + '/branch/' + branch;
+    }
+
+    throw 'Invalid family';
   }
 
   module.exports = function(entries) {
     var commits = _.map(entries, function(entry) {
       var e = JSON.parse(entry.data);
-      var repoInfo = getRepoInfo(e.html_url);
       var family = getFamily(entry.eventType);
+      var repoInfo = getRepoInfo(family, e.html_url);
 
       return {
         commitDate: e.commit.committer.date,
@@ -48,7 +67,7 @@
         commitHref: e.html_url,
         repo: repoInfo.repoOwner + '/' + repoInfo.repoName,
         branch: e.branch,
-        branchHref: getBranchHref(getRepoHref(repoInfo), e.branch),
+        branchHref: getBranchHref(family, getRepoHref(repoInfo), e.branch),
         repoHref: getRepoHref(repoInfo)
       };
     });
