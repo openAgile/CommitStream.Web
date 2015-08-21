bf35b00a7a9b7b583391f7a8866b272deace5533
kunzimariano <kunzi.mariano@gmail.com>
2015-08-21 10:11:13 -0300

    S-53161 added translatePush function to bitbucketTranslator.

--- src/app/api/translators/es6/bitbucketTranslator.js
+++ src/app/api/translators/es6/bitbucketTranslator.js
@@ -14,4 +14,50 @@
     return false;
   }
 
+  bitbucketTranslator.translatePush = (pushEvent, instanceId, digestId, inboxId) => {
+
+    try {
+      let branch = pushEvent.push.changes[0].new.name;
+      let repository = {
+        id: pushEvent.repository.uuid,
+        name: pushEvent.repository.name
+      };
+      let events = _.map(pushEvent.push.changes[0].commits, function(aCommit) {
+        let commit = {
+          sha: aCommit.hash,
+          commit: {
+            author: aCommit.author.user.username,
+            // bitbucket does not have a commit.committer object. Using the same thing as author for now.
+            committer: {
+              name: aCommit.author.user.display_name,
+              email: aCommit.author.raw,
+              date: ''
+            },
+            message: aCommit.message
+          },
+          html_url: aCommit.links.html.href,
+          repository: repository,
+          branch: branch,
+          originalMessage: aCommit
+        };
+        return {
+          eventId: uuid(),
+          eventType: 'BitbucketCommitReceived',
+          data: commit,
+          metadata: {
+            instanceId: instanceId,
+            digestId: digestId,
+            inboxId: inboxId
+          }
+        };
+      });
+
+      return events;
+    } catch (ex) {
+      console.log(ex);
+      throw ex;
+      //throw new BitbucketCommitMalformedError(ex, pushEvent);
+    }
+  }
+
 })(module.exports);
