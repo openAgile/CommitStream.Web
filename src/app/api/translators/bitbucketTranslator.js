7c56bc5a26759146611882fc9af05466116b0278
JogoShugh <jsgough>
2015-08-21 15:58:18 -0400

    S-53161 ES2015 syntactical sugaring suggestions

--- src/app/api/translators/bitbucketTranslator.js
+++ src/app/api/translators/bitbucketTranslator.js
@@ -1,70 +1,80 @@
 'use strict';
 
-(function (bitbucketTranslator) {
-  var _ = require('underscore'),
-      uuid = require('uuid-v4');
+var _interopRequireDefault = require('babel-runtime/helpers/interop-require-default')['default'];
 
-  var hasCorrectHeaders = function hasCorrectHeaders(headers) {
-    //X-Event-Key: repo:push
-    return headers.hasOwnProperty('User-Agent') && headers['User-Agent'] === 'Bitbucket-Webhooks/2.0';
-  };
+Object.defineProperty(exports, '__esModule', {
+  value: true
+});
 
-  bitbucketTranslator.canTranslate = function (request) {
-    if (hasCorrectHeaders(request.headers)) {
-      return true;
-    }
-    return false;
-  };
+var _underscore = require('underscore');
 
-  bitbucketTranslator.translatePush = function (pushEvent, instanceId, digestId, inboxId) {
+var _underscore2 = _interopRequireDefault(_underscore);
 
-    try {
-      var _ret = (function () {
-        var branch = pushEvent.push.changes[0]['new'].name;
-        var repository = {
-          id: pushEvent.repository.uuid,
-          name: pushEvent.repository.name
-        };
-        var events = _.map(pushEvent.push.changes[0].commits, function (aCommit) {
-          var commit = {
-            sha: aCommit.hash,
-            commit: {
-              author: aCommit.author.user.username,
-              // bitbucket does not have a commit.committer object. Using the same thing as author for now.
-              committer: {
-                name: aCommit.author.user.display_name,
-                email: aCommit.author.raw,
-                date: ''
-              },
-              message: aCommit.message
+var _uuidV4 = require('uuid-v4');
+
+var _uuidV42 = _interopRequireDefault(_uuidV4);
+
+var bitbucketTranslator = {};
+
+//X-Event-Key: repo:push
+var hasCorrectHeaders = function hasCorrectHeaders(headers) {
+  return headers.hasOwnProperty('User-Agent') && headers['User-Agent'] === 'Bitbucket-Webhooks/2.0';
+};
+
+bitbucketTranslator.canTranslate = function (request) {
+  return hasCorrectHeaders(request.headers);
+};
+
+bitbucketTranslator.translatePush = function (pushEvent, instanceId, digestId, inboxId) {
+  try {
+    var _ret = (function () {
+      var branch = pushEvent.push.changes[0]['new'].name;
+      var repository = {
+        id: pushEvent.repository.uuid,
+        name: pushEvent.repository.name
+      };
+      var events = _underscore2['default'].map(pushEvent.push.changes[0].commits, function (aCommit) {
+        var commit = {
+          sha: aCommit.hash,
+          commit: {
+            author: aCommit.author.user.username,
+            // bitbucket does not have a commit.committer object. Using the same thing as author for now.
+            committer: {
+              name: aCommit.author.user.display_name,
+              email: aCommit.author.raw,
+              date: ''
             },
-            html_url: aCommit.links.html.href,
-            repository: repository,
-            branch: branch,
-            originalMessage: aCommit
-          };
-          return {
-            eventId: uuid(),
-            eventType: 'BitbucketCommitReceived',
-            data: commit,
-            metadata: {
-              instanceId: instanceId,
-              digestId: digestId,
-              inboxId: inboxId
-            }
-          };
-        });
-
-        return {
-          v: events
+            message: aCommit.message
+          },
+          html_url: aCommit.links.html.href,
+          repository: repository,
+          branch: branch,
+          originalMessage: aCommit
         };
-      })();
+        return {
+          eventId: (0, _uuidV42['default'])(),
+          eventType: 'BitbucketCommitReceived',
+          data: commit,
+          metadata: {
+            instanceId: instanceId,
+            digestId: digestId,
+            inboxId: inboxId
+          }
+        };
+      });
 
-      if (typeof _ret === 'object') return _ret.v;
-    } catch (ex) {
-      console.log(ex);
-      throw ex;
-      //throw new BitbucketCommitMalformedError(ex, pushEvent);
-    }
-  };
-})(module.exports);
+      return {
+        v: events
+      };
+    })();
+
+    if (typeof _ret === 'object') return _ret.v;
+  } catch (ex) {
+    console.log(ex);
+    throw ex;
+    //throw new BitbucketCommitMalformedError(ex, pushEvent);
+  }
+};
+
+exports['default'] = bitbucketTranslator;
+module.exports = exports['default'];
--- src/app/api/translators/es6/bitbucketTranslator.js
+++ src/app/api/translators/es6/bitbucketTranslator.js
@@ -1,63 +1,56 @@
-((bitbucketTranslator) => {
-  let _ = require('underscore'),
-    uuid = require('uuid-v4');
+import _ from 'underscore';
+import uuid from 'uuid-v4';
 
-  let hasCorrectHeaders = (headers) => {
-    //X-Event-Key: repo:push
-    return headers.hasOwnProperty('User-Agent') && headers['User-Agent'] === 'Bitbucket-Webhooks/2.0';
-  }
+let bitbucketTranslator = {};
 
-  bitbucketTranslator.canTranslate = (request) => {
-    if (hasCorrectHeaders(request.headers)) {
-      return true;
-    }
-    return false;
-  }
+//X-Event-Key: repo:push
+let hasCorrectHeaders = headers => headers.hasOwnProperty('User-Agent') && headers['User-Agent'] === 'Bitbucket-Webhooks/2.0';
 
-  bitbucketTranslator.translatePush = (pushEvent, instanceId, digestId, inboxId) => {
+bitbucketTranslator.canTranslate = request => hasCorrectHeaders(request.headers);
 
-    try {
-      let branch = pushEvent.push.changes[0].new.name;
-      let repository = {
-        id: pushEvent.repository.uuid,
-        name: pushEvent.repository.name
-      };
-      let events = _.map(pushEvent.push.changes[0].commits, function(aCommit) {
-        let commit = {
-          sha: aCommit.hash,
-          commit: {
-            author: aCommit.author.user.username,
-            // bitbucket does not have a commit.committer object. Using the same thing as author for now.
-            committer: {
-              name: aCommit.author.user.display_name,
-              email: aCommit.author.raw,
-              date: ''
-            },
-            message: aCommit.message
+bitbucketTranslator.translatePush = (pushEvent, instanceId, digestId, inboxId) => {
+  try {
+    let branch = pushEvent.push.changes[0].new.name;
+    let repository = {
+      id: pushEvent.repository.uuid,
+      name: pushEvent.repository.name
+    };
+    let events = _.map(pushEvent.push.changes[0].commits, aCommit => {
+      let commit = {
+        sha: aCommit.hash,
+        commit: {
+          author: aCommit.author.user.username,
+          // bitbucket does not have a commit.committer object. Using the same thing as author for now.
+          committer: {
+            name: aCommit.author.user.display_name,
+            email: aCommit.author.raw,
+            date: ''
           },
-          html_url: aCommit.links.html.href,
-          repository: repository,
-          branch: branch,
-          originalMessage: aCommit
-        };
-        return {
-          eventId: uuid(),
-          eventType: 'BitbucketCommitReceived',
-          data: commit,
-          metadata: {
-            instanceId: instanceId,
-            digestId: digestId,
-            inboxId: inboxId
-          }
-        };
-      });
+          message: aCommit.message
+        },
+        html_url: aCommit.links.html.href,
+        repository,
+        branch,
+        originalMessage: aCommit
+      };
+      return {
+        eventId: uuid(),
+        eventType: 'BitbucketCommitReceived',
+        data: commit,
+        metadata: {
+          instanceId,
+          digestId,
+          inboxId
+        }
+      };
+    });
 
-      return events;
-    } catch (ex) {
-      console.log(ex);
-      throw ex;
-      //throw new BitbucketCommitMalformedError(ex, pushEvent);
-    }
+    return events;
+  } catch (ex) {
+    console.log(ex);
+    throw ex;
+    //throw new BitbucketCommitMalformedError(ex, pushEvent);
   }
+};
 
-})(module.exports);
+export default bitbucketTranslator;
