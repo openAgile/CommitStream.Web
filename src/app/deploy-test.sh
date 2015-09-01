4c29dba9a345c32f4f4d6c63563c491831f50c73
Daniel <daniel.gruesso@gmail.com>
2015-09-01 11:49:14 -0400

    S-54908 updated test and demo deploy files from http to https.

--- src/app/deploy-test.sh
+++ src/app/deploy-test.sh
@@ -1,3 +1,3 @@
 sourceBranch=`git branch | awk '/^\*/{print $2}'`
-echo "Pushing current branch '$sourceBranch' onto the 'v1-cs-test' branch for testing on http://v1-cs-test.azurewebsites.net"
+echo "Pushing current branch '$sourceBranch' onto the 'v1-cs-test' branch for testing on https://v1-cs-test.azurewebsites.net"
 grunt gitpush:deploy-test --source=$sourceBranch
