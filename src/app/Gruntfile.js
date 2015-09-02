d8c980b753e7d3f3a49c2f8f8dcbde465fb520b4
Josh Gough <jsgough@gmail.com>
2015-09-02 13:24:39 -0400

    Update Gruntfile.js

--- src/app/Gruntfile.js
+++ src/app/Gruntfile.js
@@ -90,7 +90,14 @@ module.exports = function(grunt) {
           branch: sourceBranch + ':v1cs-demo',
           force: true
         }
-      }
+      },
+      'deploy-dev': {
+        options: {
+          // Note: this gets replaced by the event below...
+          branch: sourceBranch + ':v1cs-dev',
+          force: true
+        }
+      }    
     }
   });
 
@@ -102,6 +109,7 @@ module.exports = function(grunt) {
   grunt.event.on('gitpush', function(action) {
     grunt.config('gitpush.deploy-test.branch', sourceBranch + ':v1-cs-test');
     grunt.config('gitpush.deploy-demo.branch', sourceBranch + ':v1cs-demo');
+    grunt.config('gitpush.deploy-dev.branch', sourceBranch + ':v1cs-dev');
   });
 
   grunt.registerTask('dev', ['less', 'babel', 'express', 'watch']);
