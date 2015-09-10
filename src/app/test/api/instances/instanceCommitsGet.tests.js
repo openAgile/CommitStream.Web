35d3e8d091be6d4a158522b9c2f3a45b6ede9b1f
kunzimariano <kunzi.mariano@gmail.com>
2015-09-10 13:51:06 -0300

    First test cases for instanceCommitsGet. S-54934

---
+++ src/app/test/api/instances/instanceCommitsGet.tests.js
@@ -0,0 +1,68 @@
+require('../handler-base')();
+
+var cache = sinon.spy();
+var cacheCreate = sinon.stub().returns(cache);
+var commitsSpy = sinon.spy();
+var commitsGet = sinon.stub().resolves(commitsSpy);
+var commitsChildrenGet = sinon.stub().resolves(commitsSpy);
+
+var handler = proxyquire('../../api/instances/instanceCommitsGet', {
+  '../helpers/commitsGet': commitsGet,
+  '../helpers/cacheCreate': cacheCreate,
+  '../helpers/commitsChildrenGet': commitsChildrenGet
+});
+
+function createRequest() {
+  var request = httpMocks.createRequest({
+    method: 'GET',
+    url: '/api/instances/instanceCommitsGet'
+  });
+  request.href = sinon.spy();
+  request.query = sinon.spy();
+  request.instance = {
+    instanceId: '1234'
+  };
+  request.params = {
+    workitems: ''
+  };
+  return request;
+}
+
+function createResponse() {
+  var response = httpMocks.createResponse();
+  response.hal = sinon.spy();
+  response.send = sinon.spy();
+  return response;
+}
+
+describe('instanceCommitsGet', function() {
+
+  describe('when getting commits from an instance for a single asset', function() {
+    var request = createRequest();
+    var response = createResponse();
+    request.params.workitems = 'S-00011';
+
+    handler(request, response);
+
+    var stream = 'versionOne_CommitsWithWorkitems-1234_S-00011';
+
+    it('should call commitsGet with the proper arguments', function() {
+      commitsGet.should.have.been.calledWith(request.query, stream, sinon.match.func, cache);
+    });
+  });
+
+  describe('when getting commits from an instance for multiple assets', function() {
+    var request = createRequest();
+    var response = createResponse();
+    request.params.workitems = 'S-00001,T-00002';
+
+    handler(request, response);
+
+    var streams = ['versionOne_CommitsWithWorkitems-1234_S-00001', 'versionOne_CommitsWithWorkitems-1234_T-00002'];
+
+    it('should call commitsChildrenGet with the proper arguments', function() {
+      commitsChildrenGet.should.have.been.calledWith(request.query, streams, sinon.match.func);
+    });
+  });
+
+});
