3c9806b840a074292aacbe1b3c480cc1656faefa
matiasheffel <matiasheffel@hotmail.com>
2016-08-08 16:25:49 -0300

    S-58989 added test for svndecorator

---
+++ src/app/test/api/inboxes/halDecorators/svnDecorator.tests.js
@@ -0,0 +1,72 @@
+require('../../handler-base')();
+
+var svnDecorator = require('../../../../api/inboxes/halDecorators/svnDecorator');
+
+var basicHalResponse = {
+  "_links": {
+    "self": {
+      "href": "/api/11111111-1111-4111-1111-111111111111/inboxes/22222222-2222-4222-2222-222222222222"
+    },
+    "digest-parent": {
+      "href": "/api/11111111-1111-4111-1111-111111111111/digests/33333333-3333-4333-3333-333333333333"
+    },
+    "add-commit": {
+      "href": "/api/11111111-1111-4111-1111-111111111111/inboxes/22222222-2222-4222-2222-222222222222/commits"
+    },
+    "inbox-remove": {
+      "href": "/api/11111111-1111-4111-1111-111111111111/inboxes/22222222-2222-4222-2222-222222222222"
+    }
+  },
+  'inboxId': '22222222-2222-4222-2222-222222222222',
+  "family": 'a random family',
+  "name": 'a random name',
+  "url": 'http://random.url.com'
+};
+
+
+var expected = {
+  "_links": {
+    "self": {
+      "href": "/api/11111111-1111-4111-1111-111111111111/inboxes/22222222-2222-4222-2222-222222222222"
+    },
+    "digest-parent": {
+      "href": "/api/11111111-1111-4111-1111-111111111111/digests/33333333-3333-4333-3333-333333333333"
+    },
+    "add-commit": {
+      "href": "/api/11111111-1111-4111-1111-111111111111/inboxes/22222222-2222-4222-2222-222222222222/commits"
+    },
+    "inbox-remove": {
+      "href": "/api/11111111-1111-4111-1111-111111111111/inboxes/22222222-2222-4222-2222-222222222222"
+    }
+  },
+  'inboxId': '22222222-2222-4222-2222-222222222222',
+  "family": 'a random family',
+  "name": 'a random name',
+  "url": 'http://random.url.com',
+  "_embedded": {
+    "Svn-scripts":[{
+     "_links": {
+        "self": {
+          "href": "/api/11111111-1111-4111-1111-111111111111/inboxes/22222222-2222-4222-2222-222222222222/script?platform=windows"
+        },
+        "platform": "windows"
+        }
+      },
+      {
+        "_links": {
+          "self": {
+            "href":"/api/11111111-1111-4111-1111-111111111111/inboxes/22222222-2222-4222-2222-222222222222/script?platform=linux"
+        },
+        "platform": "linux"
+      }
+    }
+  ]}
+};
+
+describe('svnDecorator', function() {
+  describe('when calling it', function() {
+    it('should return the expected value', function() {
+      svnDecorator.decorateHalResponse(basicHalResponse).should.deep.equal(expected);
+    });
+  });
+});
