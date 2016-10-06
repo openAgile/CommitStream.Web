f34e78b3b2e661c650aabf857d888651873b8341
Ilmari Kontulainen <ilmari@deveo.com>
2016-10-06 13:04:31 +0300

    Modify install script to work.

--- install.ps1
+++ install.ps1
@@ -2,13 +2,13 @@
 choco install eventstore -source https://www.myget.org/F/versionone/ -force
 
 # Set up the default configuration to use
-sc "C:\Program Files\eventstore\config.yml" "Db: C:\Program Files\eventstore\Data`nLog: C:\Program Files\eventstore\Log`nRunProjections: ALL"
+sc "C:\ProgramData\eventstore.3.5.0\config.yml" "Db: C:\ProgramData\eventstore.3.5.0\Data`nLog: C:\ProgramData\eventstore.3.5.0\Log`nRunProjections: ALL"
 
 # Turn EventStore into a Windows service with that obeys the above configuration settings
-nssm install eventstore "C:\Program Files\eventstore\EventStore.ClusterNode.exe" --config C:\PROGRA~1\eventstore\config.yml
+nssm install eventstore "C:\ProgramData\eventstore.3.5.0\EventStore.ClusterNode.exe" --config C:\ProgramData\eventstore.3.5.0\config.yml
 nssm start eventstore
 
 # Start your engines
 cd src/app
-npm install
+npm install --no-optional
 start npm start
