99a8a37ff0eda7a32ea1916577a2c685fdef9984
darchitetto <denise@digital-pioneers.com>
2016-10-12 11:46:13 -0400

    S-59386 added the p4v resource file and moved the svn resource file under the correct family folder

---
+++ src/app/api/inboxes/resources/p4v/commit-event.ps1
@@ -0,0 +1,49 @@
+Param(
+[string]$change,
+[string]$user,
+[string]$client,
+[string]$server
+)
+
+$p4AdminUser = "ADMINISTRATOR CREDENTIALS HERE"
+$p4AdminPass = "ADMINISTRATOR CREDENTIALS HERE"
+
+$repository = "PLACE REPO URL HERE"
+$html_url = "PLACE BASE URL TO INSPECT YOUR REVISIONS"
+$endpoint = "PLACE INBOX URL HERE"
+$headers = @{"CS-P4V-Event"="Commit Event"}
+
+$changeListEvent = @()
+
+$changeListEvent = (P4 -c $client -p $server -u $p4AdminUser -P $p4AdminPass describe -s -f "$change") -split '[\r\n]'
+
+$ndx = [array]::IndexOf($changeListEvent, 'Affected files ...')
+$affectedFiles = @()
+
+for($i = $ndx+1; $i -lt $changeListEvent.Length; $i++) {
+    if($changeListEvent[$i] -ne '') {
+        $affectedFiles += $changeListEvent[$i]
+    }
+}
+
+$message = $changeListEvent[2..($ndx-2)]
+
+$authorDate = ($changeListEvent[0] -split ' ') |? {$_ -ne ''}
+
+$author = $authorDate[3]
+$date = $authorDate[5] + ' ' + $authorDate[6]
+
+$payload = @{
+    author = "$author"
+    committer = @{
+        name = "$author"
+        date = "$date"
+    }
+    revision = "$change"
+    message = "$message"
+    repository = "$repository"
+    html_url = "$($html_url)$($revision)"
+    changes = $affectedFiles
+}
+$json = (ConvertTo-Json $payload -Depth 99)
+Invoke-RestMethod -H $headers -Uri $endpoint -Method Post -ContentType "application/json" -Body $json
---
+++ src/app/api/inboxes/resources/p4v/commit-event.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+#The next 3 line should be added in post-commit file under hooks folder
+#REPOS="$1"
+#REV="$2"
+#"$REPOS"/hooks/commit-event.sh "$REPOS" "$REV"
+
+# BASH 4.0 or higher is required
+# THIS IS SO WE CAN USE MAPFILE
+
+p4AdminUser="ADMINISTRATOR CREDENTIALS HERE"
+p4AdminPass="ADMINISTRATOR CREDENTIALS HERE"
+
+REVISION="$1"
+user="$2"
+client="$3"
+server="$4"
+
+REPOSITORY="PLACE REPO URL HERE"
+HTML_URL="PLACE BASE URL TO INSPECT YOUR REVISIONS"
+ENDPOINT="PLACE INBOX URL HERE"
+
+COMMITEVENTHEADER="CS-P4V-Event:Commit Event"
+APPJSONHEADER="Content-Type:application/json"
+
+#THIS SHOULD BE THE PATH FOR P4 EXECUTABLE
+p4="/bin/p4"
+
+result=`$p4 -c $client -p $server -u $p4AdminUser -P $p4AdminPass describe -s -f $REVISION`
+mapfile -t splitResult <<< "$result"
+
+indexOfAffectedFiles=0
+index=0
+    while [ "$index" -lt "${#splitResult[@]}" ]; do
+        if [ "${splitResult[$index]}" = "Affected files ..." ]; then
+            indexOfAffectedFiles=$index
+            return
+        fi
+        let "index++"
+    done
+
+otherIndex=$((indexOfAffectedFiles+1))
+    while [ "$otherIndex" -lt "${#splitResult[@]}" ]; do
+        if [[ "${splitResult[$otherIndex]}" = *[!\ ]* ]]; then
+            CHANGESFORJSON+=\""${splitResult[$otherIndex]}\","
+        fi
+        let "otherIndex++"
+    done
+CHANGESFORJSON="${CHANGESFORJSON::-1}"
+
+LOG=("${splitResult[@]:2:((indexOfAffectedFiles-3))}")
+
+LOG="${LOG[@]}"
+LOG=${LOG//[[:space:]]/' '}
+
+authorDate="${splitResult[0]}"
+authorDate=( $authorDate )
+
+WHO="${authorDate[3]}"
+WHEN="${authorDate[5]} ${authorDate[6]}"
+
+PAYLOAD='{"pretext":"Commit completed: rev. '"$REVISION"'","committer":{"name":"'"$WHO"'","date":"'"$WHEN"'"},"author":"'"$WHO"'","revision":"'"$REVISION"'","message":"'"$LOG"'","changes":['"$CHANGESFORJSON"'], "repository":"'"$REPOSITORY"'", "html_url":"'"$HTML_URL$REVISION"'"}'
+
+curl -X POST -H "$COMMITEVENTHEADER" -H "$APPJSONHEADER" --data "$PAYLOAD" "$ENDPOINT"
