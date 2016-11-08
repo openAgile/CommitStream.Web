6d60b80de6b1a70680a3386916fe01930767afba
matiasheffel <matiasheffel@hotmail.com>
2016-11-08 15:00:37 -0300

    D-11289 fixed projection to just link one mention for each asset id

--- src/app/projections/partition-commits-for-instance-by-versionone-workitem.js
+++ src/app/projections/partition-commits-for-instance-by-versionone-workitem.js
@@ -8,7 +8,11 @@ var callback = function(state, ev) {
   if (ev.eventType[0] != '$' && ev.metadata && ev.metadata.instanceId 
       && ev.data && ev.data.commit && ev.data.commit.message) {
     var workItems = getWorkitems(ev.data.commit.message);
-    workItems.forEach(function(workItem) {
+    var workItems = getWorkitems(ev.data.commit.message);
+    var withOutDuplicates = workItems.filter(function (i, p) {
+      return workItems.indexOf(i) == p;
+    });
+    withOutDuplicates.forEach(function(workItem) {
       workItem = workItem.toUpperCase();
       linkTo('versionOne_CommitsWithWorkitems-' + ev.metadata.instanceId + '_' + workItem, ev);
     });
