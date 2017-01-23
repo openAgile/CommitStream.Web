7697a4638f0b416ff8145634304ad2490f115753
darchitetto <denise@digital-pioneers.com>
2017-01-23 16:11:34 -0500

    S-61036 Convert InstanceAdded

--- src/app/api/instances/es6/instanceAdded.js
+++ src/app/api/instances/es6/instanceAdded.js
@@ -1,19 +1,17 @@
-'use strict';
+(((instanceAdded) => {
+    const uuid = require('uuid-v4')
+    const schemaValidator = require('../schemaValidator');
 
-(function (instanceAdded) {
-    var uuid = require('uuid-v4');
-    var schemaValidator = require('../schemaValidator');
-
-    instanceAdded.create = function () {
-        var eventId = uuid();
-        var instanceId = uuid();
-        var apiKey = uuid();
-        var instanceAddedEvent = {
+    instanceAdded.create = function() {
+        const eventId = uuid();
+        const instanceId = uuid();
+        const apiKey = uuid();
+        const instanceAddedEvent = {
             eventType: 'InstanceAdded',
             eventId: eventId,
             data: {
                 instanceId: instanceId,
-                apiKey: apiKey
+                apiKey: apiKey,
             }
         };
 
@@ -23,7 +21,9 @@
     instanceAdded.SCHEMA = {
         "title": "instance",
         "type": "object",
-        "required": ["instanceId"],
+        "required": [
+            "instanceId"
+        ],
         "properties": {
             "instanceId": {
                 "title": "ID of the instance",
@@ -40,7 +40,8 @@
         }
     };
 
-    instanceAdded.validate = function (data) {
+    instanceAdded.validate = function(data) {
         return schemaValidator.validate('instance', data, instanceAdded.SCHEMA);
     };
-})(module.exports);
+
+}))(module.exports);
