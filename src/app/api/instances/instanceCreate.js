1c6ad1a80189d0230edf9c2f549b3e0d1d5281b5
darchitetto <denise@digital-pioneers.com>
2017-01-23 15:33:24 -0500

    S-61036 Convert instance create

--- src/app/api/instances/instanceCreate.js
+++ src/app/api/instances/instanceCreate.js
@@ -1,24 +1,40 @@
-(function() {
-  var instanceAdded = require('./instanceAdded'),
-      instanceFormatAsHal = require('./instanceFormatAsHal'),
-      eventStore = require('../helpers/eventStoreClient'),
-      setTimeout = require('../helpers/setTimeout'),
-      config = require('../../config');
+'use strict';
 
-  module.exports = function(req, res, next) {
-    var instanceAddedEvent = instanceAdded.create();
+var _interopRequireDefault = require('babel-runtime/helpers/interop-require-default')['default'];
 
-    var args = {
-      name: 'instances',
-      events: instanceAddedEvent
+var _instanceFormatAsHal = require('./instanceFormatAsHal');
+
+var _instanceFormatAsHal2 = _interopRequireDefault(_instanceFormatAsHal);
+
+var _instanceAdded = require('./instanceAdded');
+
+var _instanceAdded2 = _interopRequireDefault(_instanceAdded);
+
+var _helpersEventStoreClient = require('../helpers/eventStoreClient');
+
+var _helpersEventStoreClient2 = _interopRequireDefault(_helpersEventStoreClient);
+
+var _helpersSetTimeout = require('../helpers/setTimeout');
+
+var _helpersSetTimeout2 = _interopRequireDefault(_helpersSetTimeout);
+
+var _config = require('../../config');
+
+var _config2 = _interopRequireDefault(_config);
+
+(function () {
+    module.exports = function (req, res) {
+        var instanceAddedEvent = _instanceAdded2['default'].create();
+        var args = {
+            name: 'instances',
+            events: instanceAddedEvent
+        };
+
+        _helpersEventStoreClient2['default'].postToStream(args).then(function () {
+            var hypermedia = (0, _instanceFormatAsHal2['default'])(req.href, instanceAddedEvent.data);
+            (0, _helpersSetTimeout2['default'])(function () {
+                res.hal(hypermedia, 201);
+            }, _config2['default'].controllerResponseDelay);
+        });
     };
-
-    eventStore.postToStream(args)
-      .then(function() {
-        var hypermedia = instanceFormatAsHal(req.href, instanceAddedEvent.data);
-        setTimeout(function() {
-          res.hal(hypermedia, 201);
-        }, config.controllerResponseDelay);
-      });
-  };
-}())
+})();
