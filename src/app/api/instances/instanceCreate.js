2911d7ec6528615d8b016ac086672e3383ed47cc
matiasheffel <matiasheffel@hotmail.com>
2017-01-24 16:29:27 -0300

    S-61036 refactor es6 using export default

--- src/app/api/instances/instanceCreate.js
+++ src/app/api/instances/instanceCreate.js
@@ -2,6 +2,10 @@
 
 var _interopRequireDefault = require('babel-runtime/helpers/interop-require-default')['default'];
 
+Object.defineProperty(exports, '__esModule', {
+    value: true
+});
+
 var _instanceFormatAsHal = require('./instanceFormatAsHal');
 
 var _instanceFormatAsHal2 = _interopRequireDefault(_instanceFormatAsHal);
@@ -22,19 +26,18 @@ var _config = require('../../config');
 
 var _config2 = _interopRequireDefault(_config);
 
-(function () {
-    module.exports = function (req, res) {
-        var instanceAddedEvent = _instanceAdded2['default'].create();
-        var args = {
-            name: 'instances',
-            events: instanceAddedEvent
-        };
-
-        _helpersEventStoreClient2['default'].postToStream(args).then(function () {
-            var hypermedia = (0, _instanceFormatAsHal2['default'])(req.href, instanceAddedEvent.data);
-            (0, _helpersSetTimeout2['default'])(function () {
-                res.hal(hypermedia, 201);
-            }, _config2['default'].controllerResponseDelay);
-        });
+exports['default'] = function (req, res) {
+    var instanceAddedEvent = _instanceAdded2['default'].create();
+    var args = {
+        name: 'instances',
+        events: instanceAddedEvent
     };
-})();
+    _helpersEventStoreClient2['default'].postToStream(args).then(function () {
+        var hypermedia = (0, _instanceFormatAsHal2['default'])(req.href, instanceAddedEvent.data);
+        (0, _helpersSetTimeout2['default'])(function () {
+            res.hal(hypermedia, 201);
+        }, _config2['default'].controllerResponseDelay);
+    });
+};
+
+module.exports = exports['default'];
