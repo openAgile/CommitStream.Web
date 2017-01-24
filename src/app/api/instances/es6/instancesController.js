1e5f6389a870bfbc6d378e96603cf6e3823fe190
jogoshugh <jsgough@gmail.com>
2017-01-24 10:59:15 -0500

    S-61036 Adjusted the ES2015 syntax for a couple of files

--- src/app/api/instances/es6/instancesController.js
+++ src/app/api/instances/es6/instancesController.js
@@ -2,10 +2,10 @@ import instanceCreate from './instanceCreate';
 import instanceGet from './instanceCommitsGet';
 import instanceCommitGet from './instanceCommitsGet';
 
-((instancesController => {
-	instancesController.init = (app) => {
-    app.post('/api/instances', instanceCreate);
-    app.get('/api/instances/:instanceId', instanceGet);
-    app.get('/api/:instanceId/commits/tags/versionone/workitem', instanceCommitGet);
-  };
-})(module.exports))
+export default {
+	init(app) {
+	    app.post('/api/instances', instanceCreate);
+	    app.get('/api/instances/:instanceId', instanceGet);
+	    app.get('/api/:instanceId/commits/tags/versionone/workitem', instanceCommitGet);
+	}
+};
--- src/app/api/instances/instancesController.js
+++ src/app/api/instances/instancesController.js
@@ -2,6 +2,10 @@
 
 var _interopRequireDefault = require('babel-runtime/helpers/interop-require-default')['default'];
 
+Object.defineProperty(exports, '__esModule', {
+	value: true
+});
+
 var _instanceCreate = require('./instanceCreate');
 
 var _instanceCreate2 = _interopRequireDefault(_instanceCreate);
@@ -12,10 +16,11 @@ var _instanceCommitsGet2 = _interopRequireDefault(_instanceCommitsGet);
 
 var _instanceCommitsGet3 = _interopRequireDefault(_instanceCommitsGet);
 
-(function (instancesController) {
-   instancesController.init = function (app) {
-      app.post('/api/instances', _instanceCreate2['default']);
-      app.get('/api/instances/:instanceId', _instanceCommitsGet2['default']);
-      app.get('/api/:instanceId/commits/tags/versionone/workitem', _instanceCommitsGet3['default']);
-   };
-})(module.exports);
+exports['default'] = {
+	init: function init(app) {
+		app.post('/api/instances', _instanceCreate2['default']);
+		app.get('/api/instances/:instanceId', _instanceCommitsGet2['default']);
+		app.get('/api/:instanceId/commits/tags/versionone/workitem', _instanceCommitsGet3['default']);
+	}
+};
+module.exports = exports['default'];
