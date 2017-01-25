d005a772f99f080111e03169b72aeaf7db2a93b5
jogoshugh <jsgough@gmail.com>
2017-01-25 13:02:42 -0500

    S-61014 Created api/es6/sanitizer.js

--- src/app/api/sanitizer.js
+++ src/app/api/sanitizer.js
@@ -1,21 +1,54 @@
-(function(sanitizer) {
-	var sanitizeHtml = require('sanitize-html');
+'use strict';
 
-	sanitizer.sanitize = function(typeName, obj, fields) {
-		var errors = [];
-		
-		fields.forEach(function(field) {
-			if (obj.hasOwnProperty(field)) {
-				var originalValue = obj[field];
-				var sanitizedValue = sanitizeHtml(obj[field], {allowedTags: []});
-				obj[field] = sanitizedValue;
-				if (originalValue !== sanitizedValue) {
-					errors.push(typeName + '.' + field + ' cannot contain script tags or HTML.');
-				}
-			}
-		});
-		
-		return errors;
-	};
+var _getIterator = require('babel-runtime/core-js/get-iterator')['default'];
 
-})(module.exports);
+var _interopRequireDefault = require('babel-runtime/helpers/interop-require-default')['default'];
+
+Object.defineProperty(exports, '__esModule', {
+  value: true
+});
+
+var _sanitizeHtml = require('sanitize-html');
+
+var _sanitizeHtml2 = _interopRequireDefault(_sanitizeHtml);
+
+exports['default'] = {
+  sanitize: function sanitize(typeName, obj, fields) {
+    var errors = [];
+
+    var _iteratorNormalCompletion = true;
+    var _didIteratorError = false;
+    var _iteratorError = undefined;
+
+    try {
+      for (var _iterator = _getIterator(fields), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+        var field = _step.value;
+
+        if (obj.hasOwnProperty(field)) {
+          var originalValue = obj[field];
+          var sanitizedValue = (0, _sanitizeHtml2['default'])(obj[field], { allowedTags: [] });
+          obj[field] = sanitizedValue;
+          if (originalValue !== sanitizedValue) {
+            errors.push(typeName + '.' + field + ' cannot contain script tags or HTML.');
+          }
+        }
+      }
+    } catch (err) {
+      _didIteratorError = true;
+      _iteratorError = err;
+    } finally {
+      try {
+        if (!_iteratorNormalCompletion && _iterator['return']) {
+          _iterator['return']();
+        }
+      } finally {
+        if (_didIteratorError) {
+          throw _iteratorError;
+        }
+      }
+    }
+
+    return errors;
+  }
+};
+module.exports = exports['default'];
