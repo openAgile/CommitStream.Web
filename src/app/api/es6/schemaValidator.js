2c5f07a8866c225aa63f55e1a797313915901357
jogoshugh <jsgough@gmail.com>
2017-01-25 13:23:56 -0500

    S-61014 Created api/es6/schemaValidator.js

---
+++ src/app/api/es6/schemaValidator.js
@@ -0,0 +1,15 @@
+import jsonschema from 'jsonschema';
+
+const validator = new jsonschema.Validator;
+
+export default {
+	validate(typeName, data, schema) {
+		const result = validator.validate(data, schema, {propertyName: typeName});
+    const errors = result.errors.map(error => {
+      console.log(error);
+      return `${error.property} ${error.message}`;
+    });
+
+    return errors;
+	}
+};
--- src/app/api/schemaValidator.js
+++ src/app/api/schemaValidator.js
@@ -1,15 +1,26 @@
-(function(schemaValidator) {
-	var _ = require('underscore'),
-		jsonschema = new (require('jsonschema').Validator);
+'use strict';
 
-	schemaValidator.validate = function(typeName, data, schema) {
-		var result = jsonschema.validate(data, schema, {propertyName: typeName});
+var _interopRequireDefault = require('babel-runtime/helpers/interop-require-default')['default'];
 
-		var errors = _.map(result.errors, function(error) {
-			console.log(error);
-			return error.property + ' ' + error.message;
-		});
+Object.defineProperty(exports, '__esModule', {
+  value: true
+});
 
-		return errors;
-	};
-})(module.exports);
+var _jsonschema = require('jsonschema');
+
+var _jsonschema2 = _interopRequireDefault(_jsonschema);
+
+var validator = new _jsonschema2['default'].Validator();
+
+exports['default'] = {
+  validate: function validate(typeName, data, schema) {
+    var result = validator.validate(data, schema, { propertyName: typeName });
+    var errors = result.errors.map(function (error) {
+      console.log(error);
+      return error.property + ' ' + error.message;
+    });
+
+    return errors;
+  }
+};
+module.exports = exports['default'];
