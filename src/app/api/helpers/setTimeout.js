e933f6dc56962e12fd636d603f9f234c9e1996eb
Pablo Freiberg <pablo.freiberg@gmail.com>
2017-01-25 16:03:44 -0300

    S-61034: converted setTimeout

--- src/app/api/helpers/setTimeout.js
+++ src/app/api/helpers/setTimeout.js
@@ -1,5 +1,11 @@
-(function() {
-  module.exports = function(callback, delay) {
-    setTimeout(callback, delay);
-  };
-}());
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+exports["default"] = function (callback, delay) {
+  return setTimeout(callback, delay);
+};
+
+module.exports = exports["default"];
