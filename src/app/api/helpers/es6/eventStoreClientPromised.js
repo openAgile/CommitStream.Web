e17d9e1d3ea4d34daba7466e77c4aee6a7be0c10
Pablo Freiberg <pablo.freiberg@gmail.com>
2017-01-25 15:29:42 -0300

    S-61034: converted eventStoreClientPromised

---
+++ src/app/api/helpers/es6/eventStoreClientPromised.js
@@ -0,0 +1,15 @@
+import EventStore from 'eventstore-client';
+import Promise from 'bluebird';
+import config from '../../config';
+
+var client = new EventStore({
+  baseUrl: config.eventStoreBaseUrl,
+  username: config.eventStoreUser,
+  password: config.eventStorePassword
+});
+
+Promise.promisifyAll(client.streams);
+Promise.promisifyAll(client.projections);
+Promise.promisifyAll(client.projection);
+
+export default client;
