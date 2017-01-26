b1a1bc8056578ab96d9e82f08e472605a889e450
Mark Irvin <mark.irvin@versionone.com>
2017-01-26 12:29:42 -0500

    S-61032 Created api/digests/es6/digestGet.js

--- src/app/api/digests/digestGet.js
+++ src/app/api/digests/digestGet.js
@@ -1,12 +1,24 @@
-(function() {
-  var digestFormatAsHal = require('./digestFormatAsHal'),
-    validateUUID = require('../validateUUID');
+'use strict';
 
-  module.exports = function(req, res, next) {
-    validateUUID('digests', req.params.digestId);
+var _interopRequireDefault = require('babel-runtime/helpers/interop-require-default')['default'];
 
-    var hypermedia = digestFormatAsHal(req.href, req.params.instanceId, req.digest);
+Object.defineProperty(exports, '__esModule', {
+    value: true
+});
+
+var _digestFormatAsHal = require('./digestFormatAsHal');
+
+var _digestFormatAsHal2 = _interopRequireDefault(_digestFormatAsHal);
+
+var _validateUUID = require('../validateUUID');
+
+var _validateUUID2 = _interopRequireDefault(_validateUUID);
+
+exports['default'] = function (req, res, next) {
+    (0, _validateUUID2['default'])('digests', req.params.digestId);
+
+    var hypermedia = (0, _digestFormatAsHal2['default'])(req.href, req.params.instanceId, req.digest);
     res.hal(hypermedia);
+};
 
-  };
-}());
+module.exports = exports['default'];
