fe9da6766017bcda5e1a7f2996e796f23a14679b
jogoshugh <jsgough@gmail.com>
2017-01-26 14:34:57 -0500

    S-61014 Created es6/server.js

--- src/app/server.js
+++ src/app/server.js
@@ -1,72 +1,130 @@
-var express = require('express'),
-  app = express(),
-  cors = require('cors'),
-  config = require('./config'),
-  exphbs = require('express-handlebars'),
-  validation = require('./configValidation'),
-  csErrorHandler = require('./middleware/csErrorHandler'),
-  instanceAuthenticator = require('./middleware/instanceAuthenticator'),
-  instanceToDigestValidator = require('./middleware/instanceToDigestValidator'),
-  instanceToInboxValidator = require('./middleware/instanceToInboxValidator'),
-  apiRoutesRequireContentTypeAppJson = require('./middleware/apiRoutesRequireContentTypeAppJson'),
-  appConfigure = require('./middleware/appConfigure'),
-  Promise = require('bluebird'),
-  domainMiddleware = require('express-domain-middleware'),
-  methodOverride = require('method-override');
+'use strict';
+
+var _interopRequireDefault = require('babel-runtime/helpers/interop-require-default')['default'];
+
+var _express = require('express');
+
+var _express2 = _interopRequireDefault(_express);
+
+var _cors = require('cors');
+
+var _cors2 = _interopRequireDefault(_cors);
+
+var _config = require('./config');
+
+var _config2 = _interopRequireDefault(_config);
+
+var _expressHandlebars = require('express-handlebars');
+
+var _expressHandlebars2 = _interopRequireDefault(_expressHandlebars);
+
+var _configValidation = require('./configValidation');
+
+var _configValidation2 = _interopRequireDefault(_configValidation);
+
+var _middlewareCsErrorHandler = require('./middleware/csErrorHandler');
+
+var _middlewareCsErrorHandler2 = _interopRequireDefault(_middlewareCsErrorHandler);
+
+var _middlewareInstanceAuthenticator = require('./middleware/instanceAuthenticator');
+
+var _middlewareInstanceAuthenticator2 = _interopRequireDefault(_middlewareInstanceAuthenticator);
+
+var _middlewareInstanceToDigestValidator = require('./middleware/instanceToDigestValidator');
+
+var _middlewareInstanceToDigestValidator2 = _interopRequireDefault(_middlewareInstanceToDigestValidator);
+
+var _middlewareInstanceToInboxValidator = require('./middleware/instanceToInboxValidator');
+
+var _middlewareInstanceToInboxValidator2 = _interopRequireDefault(_middlewareInstanceToInboxValidator);
+
+var _middlewareApiRoutesRequireContentTypeAppJson = require('./middleware/apiRoutesRequireContentTypeAppJson');
+
+var _middlewareApiRoutesRequireContentTypeAppJson2 = _interopRequireDefault(_middlewareApiRoutesRequireContentTypeAppJson);
+
+var _middlewareAppConfigure = require('./middleware/appConfigure');
+
+var _middlewareAppConfigure2 = _interopRequireDefault(_middlewareAppConfigure);
+
+var _bluebird = require('bluebird');
+
+var _bluebird2 = _interopRequireDefault(_bluebird);
+
+var _expressDomainMiddleware = require('express-domain-middleware');
+
+var _expressDomainMiddleware2 = _interopRequireDefault(_expressDomainMiddleware);
+
+var _methodOverride = require('method-override');
+
+var _methodOverride2 = _interopRequireDefault(_methodOverride);
+
+var _api = require("./api");
+
+var _api2 = _interopRequireDefault(_api);
+
+var _bootstrapper = require('./bootstrapper');
+
+var _bootstrapper2 = _interopRequireDefault(_bootstrapper);
+
+var _apiInstancesInstancesController = require('./api/instances/instancesController');
+
+var _apiInstancesInstancesController2 = _interopRequireDefault(_apiInstancesInstancesController);
 
 // DO NOT MOVE THIS. It is here to wrap routes in a domain to catch unhandled errors
-app.use(domainMiddleware);
+
+var app = (0, _express2['default'])();
+app.use(_expressDomainMiddleware2['default']);
 
 // DO NOT MOVE THIS. It is here to handle unhandled rejected Promises cleanly
-Promise.onPossiblyUnhandledRejection(function(err) {
+_bluebird2['default'].onPossiblyUnhandledRejection(function (err) {
   throw err;
 });
 
-validation.validateConfig();
-validation.validateEventStore(function(error) {
+_configValidation2['default'].validateConfig();
+_configValidation2['default'].validateEventStore(function (error) {
   if (error) {
     throw new Error(error);
   }
 });
 
 // override with the X-HTTP-Method-Override header in the request
-app.use(methodOverride('X-HTTP-Method-Override'))
+app.use((0, _methodOverride2['default'])('X-HTTP-Method-Override'));
 
-app.get('/version', function(req, res) {
+app.get('/version', function (req, res) {
   res.json({
     version: "0.0.0"
   });
 });
 
-var api = require("./api");
-require('./bootstrapper').boot(config);
+// Boot the system, and create EventStore projections if needed
+_bootstrapper2['default'].boot(_config2['default']);
 
 // Wire up express-handlebars as the view engine for express.
-app.engine('handlebars', exphbs());
+app.engine('handlebars', (0, _expressHandlebars2['default'])());
 app.set('view engine', 'handlebars');
 
 // NOTE: Do not rearrange the order of these app.* statements becaused they
 // are crucial for the order of operations in the pipeline of middleware
 // functions!
-app.use(cors());
+app.use((0, _cors2['default'])());
 
-app.use(express.static(__dirname + '/client'));
+app.use(_express2['default']['static'](__dirname + '/client'));
 
-app.get('/instances', function(req, res) {
+app.get('/instances', function (req, res) {
   res.render('instances');
 });
 
 // Ensure that all routes with :instanceId parameters are properly authenticated
-app.param('instanceId', instanceAuthenticator);
-app.param('digestId', instanceToDigestValidator);
-app.param('inboxId', instanceToInboxValidator);
+app.param('instanceId', _middlewareInstanceAuthenticator2['default']);
+app.param('digestId', _middlewareInstanceToDigestValidator2['default']);
+app.param('inboxId', _middlewareInstanceToInboxValidator2['default']);
 
 // NOTE: See above warning. Why are you even considering moving these?
 // Think thrice.
-appConfigure(app);
+(0, _middlewareAppConfigure2['default'])(app);
 
 // SPIKE: Provide a public entry point for the API
-app.get('/api/public', function(req, res) {
+app.get('/api/public', function (req, res) {
   res.set('Content-Type', 'application/hal+json');
   res.status(200).send({
     "_links": {
@@ -89,32 +147,32 @@ app.get('/api/public', function(req, res) {
 });
 
 // Return a URL as a special header for each request
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   res.setHeader("X-CommitStream-API-Docs", "https://github.com/openAgile/CommitStream.Web");
   return next();
 });
 
 // This must happen here, before we cover additional routes with Content-Type validation
-require('./api/instances/instancesController').init(app);
+_apiInstancesInstancesController2['default'].init(app);
 
 // Ensure all API routes have consistent Content-Type validation
-app.all('/api/*', apiRoutesRequireContentTypeAppJson);
+app.all('/api/*', _middlewareApiRoutesRequireContentTypeAppJson2['default']);
 
 // Map API the routes
-api.init(app);
+_api2['default'].init(app);
 
 // DO NOT MOVE THIS. It must be here to catch unhandled errors.
-app.use(csErrorHandler);
+app.use(_middlewareCsErrorHandler2['default']);
 
-function getHostSettings(req) {
+var getHostSettings = function getHostSettings(req) {
   return {
-    protocol: config.protocol || req.protocol,
+    protocol: _config2['default'].protocol || req.protocol,
     host: req.get('host'),
     key: req.query.key
   };
-}
+};
 
-app.get('/app', function(req, res) {
+app.get('/app', function (req, res) {
   res.setHeader('Content-Type', 'application/javascript');
   var settings = getHostSettings(req);
 
@@ -122,10 +180,10 @@ app.get('/app', function(req, res) {
     apiUrl: settings.protocol + '://' + settings.host + '/api/',
     protocol: settings.protocol,
     resourcePath: settings.protocol + '://' + settings.host + '/',
-    showChildrenFeatureToggle: config.showChildrenFeatureToggle.toString()
+    showChildrenFeatureToggle: _config2['default'].showChildrenFeatureToggle.toString()
   });
 });
 
-app.listen(config.port, function() {
-  console.log('CommitStream Web Server listening on port ' + config.port);
+app.listen(_config2['default'].port, function () {
+  console.log('CommitStream Web Server listening on port ' + _config2['default'].port);
 });
