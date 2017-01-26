7ac50c81ce467f673991fd543221a2fe0bb7a6d3
Pablo Freiberg <pablo.freiberg@gmail.com>
2017-01-26 13:21:51 -0300

    S-61036: introduced async/await and replaced let with const

--- src/app/api/instances/instanceCreate.js
+++ src/app/api/instances/instanceCreate.js
@@ -1,10 +1,14 @@
 'use strict';
 
+var _regeneratorRuntime = require('babel-runtime/regenerator')['default'];
+
 var _interopRequireDefault = require('babel-runtime/helpers/interop-require-default')['default'];
 
 Object.defineProperty(exports, '__esModule', {
     value: true
 });
+
+var _this = this;
 
 var _instanceFormatAsHal = require('./instanceFormatAsHal');
 
@@ -26,18 +30,31 @@ var _config = require('../../config');
 
 var _config2 = _interopRequireDefault(_config);
 
-exports['default'] = function (req, res) {
-    var instanceAddedEvent = _instanceAdded2['default'].create();
-    var args = {
-        name: 'instances',
-        events: instanceAddedEvent
-    };
-    _helpersEventStoreClient2['default'].postToStream(args).then(function () {
-        var hypermedia = (0, _instanceFormatAsHal2['default'])(req.href, instanceAddedEvent.data);
-        (0, _helpersSetTimeout2['default'])(function () {
-            res.hal(hypermedia, 201);
-        }, _config2['default'].controllerResponseDelay);
-    });
+exports['default'] = function callee$0$0(req, res) {
+    var instanceAddedEvent, args, hypermedia;
+    return _regeneratorRuntime.async(function callee$0$0$(context$1$0) {
+        while (1) switch (context$1$0.prev = context$1$0.next) {
+            case 0:
+                instanceAddedEvent = _instanceAdded2['default'].create();
+                args = {
+                    name: 'instances',
+                    events: instanceAddedEvent
+                };
+                context$1$0.next = 4;
+                return _regeneratorRuntime.awrap(_helpersEventStoreClient2['default'].postToStream(args));
+
+            case 4:
+                hypermedia = (0, _instanceFormatAsHal2['default'])(req.href, instanceAddedEvent.data);
+
+                (0, _helpersSetTimeout2['default'])(function () {
+                    res.hal(hypermedia, 201);
+                }, _config2['default'].controllerResponseDelay);
+
+            case 6:
+            case 'end':
+                return context$1$0.stop();
+        }
+    }, null, _this);
 };
 
 module.exports = exports['default'];
