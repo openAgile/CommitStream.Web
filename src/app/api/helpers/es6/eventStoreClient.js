23368a4af571b57d63ad48ac9595a89a5c7a889f
Pablo Freiberg <pablo.freiberg@gmail.com>
2017-01-26 16:18:50 -0300

    S-61034: introduced async/await

--- src/app/api/helpers/es6/eventStoreClient.js
+++ src/app/api/helpers/es6/eventStoreClient.js
@@ -3,54 +3,58 @@ import config from '../../config';
 import _ from 'underscore';
 import statusCodeValidator from './statusCodeValidator';
 
-let client = new EventStore({
+const client = new EventStore({
   baseUrl: config.eventStoreBaseUrl,
   username: config.eventStoreUser,
   password: config.eventStorePassword
 });
 
-client.queryStatePartitionById = args => {
-  var partition = args.partition || args.name + '-' + args.id;
-  var stateArgs = {
-    name: args.name,
-    partition
-  };
+export default Object.assign(client, {
+  async queryStatePartitionById(args) {
+    const partition = args.partition || `${args.name}-${args.id}`;
+    const stateArgs = {
+      name: args.name,
+      partition
+    };
 
-  return client.projection.getStateAsync(stateArgs)
-    .then(statusCodeValidator.validateGetProjection(args.name, args.id));
-}
+    const response = await client.projection.getStateAsync(stateArgs);
 
-client.postToStream = args => {
-  // Stay immutable, bro
-  var events = args.events;
-  if (!_.isArray(events)) {
-    events = [events];
+    return statusCodeValidator.validateGetProjection(args.name, args.id)(response);
+  },
+  async postToStream(args) {
+    // Stay immutable, bro
+    let events = args.events;
+    if (!_.isArray(events)) {
+      events = [events];
+    }
+    events = JSON.stringify(events);
+
+    const postArgs = {
+      name: args.name,
+      events
+    };
+
+    const response = await client.streams.postAsync(postArgs);
+
+    return statusCodeValidator.validateStreamsPost(response);
+  },
+  async getFromStream(args) {
+    const getArgs = _.pick(args, 'name', 'count', 'pageUrl', 'embed');
+
+    const response = await client.streams.getAsync(getArgs);
+
+    return statusCodeValidator.validateGetStream(args.name)(response);
+  },
+  async queryCreate(args) {
+    const response = await client.query.postAsync(args);
+    statusCodeValidator.validateQueryCreate(response);
+  },
+  async queryGetState(args) {
+    const response = await client.query.getStateAsync(args);
+    statusCodeValidator.validateQueryGetState(response);
+  },
+  async queryGetStatus(args) {
+    const response = await client.query.getStatusAsync(args);
+    statusCodeValidator.validateQueryGetStatus(response);
   }
-  events = JSON.stringify(events);
-
-  var postArgs = {
-    name: args.name,
-    events
-  };
-
-  return client.streams.postAsync(postArgs)
-    .then(statusCodeValidator.validateStreamsPost);
-};
-
-client.getFromStream = args => {
-  var getArgs = _.pick(args, 'name', 'count', 'pageUrl', 'embed');
-
-  return client.streams.getAsync(getArgs)
-    .then(statusCodeValidator.validateGetStream(args.name));
-};
-
-client.queryCreate = args => client.query.postAsync(args)
-    .then(statusCodeValidator.validateQueryCreate);
-
-client.queryGetState = args => client.query.getStateAsync(args)
-    .then(statusCodeValidator.validateQueryGetState);
-
-client.queryGetStatus = args => client.query.getStatusAsync(args)
-    .then(statusCodeValidator.validateQueryGetStatus);
-
-export default client;
+});
