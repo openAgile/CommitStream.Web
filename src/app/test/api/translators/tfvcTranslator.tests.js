096937590b3f518c7ccb7d09e25631e6229007b3
darchitetto <denise@digital-pioneers.com>
2017-03-15 12:43:28 -0400

    S-60863 Add in the payload for a TFVC event that has more than one project with failing tests.

--- src/app/test/api/translators/tfvcTranslator.tests.js
+++ src/app/test/api/translators/tfvcTranslator.tests.js
@@ -64,6 +64,59 @@ var tfvcPushEventForOneProject = {
     "createdDate": "2017-01-20T16:28:47.755774Z"
 };
 
+var tfvcPushEventForMoreThanOneProject= {
+    "subscriptionId": "5a5419f9-8deb-46ca-8c9b-825e80311c6c",
+    "notificationId": 1,
+    "id": "b396843b-6f58-408d-b6dd-468be8d7a615",
+    "eventType": "tfvc.checkin",
+    "publisherId": "tfs",
+    "scope": "all",
+    "message": {
+        "text": "Sally Ann Cavanaugh checked in changeset 22",
+        "html": "Sally Ann Cavanaugh checked in changeset <a href=\"https://testsystem.visualstudio.com/web/cs.aspx?pcguid=80c24ec7-6164-46d7-9b2a-ab3d60d8dc71&amp;cs=22\">22</a>",
+        "markdown": "Sally Ann Cavanaugh checked in changeset [22](https://testsystem.visualstudio.com/web/cs.aspx?pcguid=80c24ec7-6164-46d7-9b2a-ab3d60d8dc71&cs=22)"
+    },
+    "detailedMessage": {
+        "text": "Sally Ann Cavanaugh checked in changeset 22",
+        "html": "Sally Ann Cavanaugh checked in changeset <a href=\"https://testsystem.visualstudio.com/web/cs.aspx?pcguid=80c24ec7-6164-46d7-9b2a-ab3d60d8dc71&amp;cs=22\">22</a>",
+        "markdown": "Sally Ann Cavanaugh checked in changeset [22](https://testsystem.visualstudio.com/web/cs.aspx?pcguid=80c24ec7-6164-46d7-9b2a-ab3d60d8dc71&cs=22)"
+    },
+    "resource": {
+        "hasMoreChanges": true,
+        "teamProjectIds": ["70cf8e3a-3ee1-4127-95d2-7f2563e5dc9e", "fdc49ee6-ec19-43a4-bd08-55800484b342"],
+        "changesetId": 22,
+        "url": "https://testsystem.visualstudio.com/_apis/tfvc/changesets/22",
+        "author": {
+            "id": "2c22451e-cd97-454e-aa61-bf076557ab3f",
+            "displayName": "Sally Ann Cavanaugh",
+            "uniqueName": "scavanaugh@nowhere.com",
+            "url": "https://thing.somewhere.visualstudio.com/A7d7713ab-3a77-4625-a298-2f17fba9310b/_apis/Identities/2c22451e-cd97-454e-aa61-bf076557ab3f",
+            "imageUrl": "https://testsystem.visualstudio.com/_api/_common/identityImage?id=2c22451e-cd97-454e-aa61-bf076557ab3f"
+        },
+        "checkedInBy": {
+            "id": "2c22451e-cd97-454e-aa61-bf076557ab3f",
+            "displayName": "Sally Ann Cavanaugh",
+            "uniqueName": "scavanaugh@nowhere.com",
+            "url": "https://thing.somewhere.visualstudio.com/A7d7713ab-3a77-4625-a298-2f17fba9310b/_apis/Identities/2c22451e-cd97-454e-aa61-bf076557ab3f",
+            "imageUrl": "https://testsystem.visualstudio.com/_api/_common/identityImage?id=2c22451e-cd97-454e-aa61-bf076557ab3f"
+        },
+        "createdDate": "2017-03-08T20:54:34Z",
+        "comment": ""
+    },
+    "resourceVersion": "1.0",
+    "resourceContainers": {
+        "collection": {
+            "id": "80c24ec7-6164-46d7-9b2a-ab3d60d8dc71",
+            "baseUrl": "https://testsystem.visualstudio.com/"
+        },
+        "account": {
+            "id": "7d7713ab-3a77-4625-a298-2f17fba9310b",
+            "baseUrl": "https://testsystem.visualstudio.com/"
+        }
+    },
+    "createdDate": "2017-03-08T20:54:37.2569716Z"
+};
+
 describe('tfvcTranslator', function() {
     var eventId = '87b66de8-8307-4e03-b2d3-da447c66501a';
     uuidStub.returns(eventId);
@@ -107,6 +160,48 @@ describe('tfvcTranslator', function() {
 
         beforeEach(function() {
             actual = tfvcTranslator.translatePush(tfvcPushEventForOneProject, instanceId, digestId, inboxId);
+        });
+
+        it('translated event should match our expected shape of data', function () {
+            actual.should.deep.equal(expected);
+        });
+
+    });
+
+    describe('when translating a push event that contains more than one commit for one project', function() {
+        var expected = [{
+            eventId: eventId,
+            eventType: "TfvcCommitReceived",
+            data: {
+                sha: "b396843b-6f58-408d-b6dd-468be8d7a615",
+                commit: {
+                    author: {
+                        name: "Sally Ann Cavanaugh",
+                        email: "scavanaugh@nowhere.com"
+                    },
+                    committer: {
+                        name: "Sally Ann Cavanaugh",
+                        email: "scavanaugh@nowhere.com",
+                        date: ''
+                    },
+                    message: "Sally Ann Cavanaugh checked in changeset 22"
+                },
+                html_url: "",
+                repository: {
+                    url: ""
+                },
+                branch: '',
+                originalMessage: tfvcPushEventForMoreThanOneProject
+            },
+            metadata: {
+                instanceId: instanceId,
+                digestId: digestId,
+                inboxId: inboxId
+            }
+        }];
+
+        beforeEach(function() {
+            actual = tfvcTranslator.translatePush(tfvcPushEventForMoreThanOneProject, instanceId, digestId, inboxId);
         });
 
         it('translated event should match our expected shape of data', function () {
