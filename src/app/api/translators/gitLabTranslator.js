946166fcabde3b996d08406c4627de629d0ec30f
Cory Wheeler <corywheeler@gmail.com>
2017-03-20 14:09:38 -0400

    S-60863 Remove commented out code.

--- src/app/api/translators/gitLabTranslator.js
+++ src/app/api/translators/gitLabTranslator.js
@@ -52,11 +52,6 @@ var gitLabTranslator = {
             commit: {
               author: aCommit.author,
               // gitLab does not have a commit.committer object. Using the same thing as author for now.
-              // committer: {
-              //   name: aCommit.committer.name,
-              //   email: aCommit.committer.email,
-              //   date: aCommit.timestamp
-              // },
               committer: {
                 name: aCommit.author.name,
                 email: aCommit.author.email,
