08eac897a6df5be1c47907fe8c5fab3d63d5d20d
Josh Gough <jsgough@gmail.com>
2018-04-06 17:54:32 -0400

    S-64842 Note about read-only flag in chunk copying

--- Azure.Migration/copy_prod_data.bat
+++ Azure.Migration/copy_prod_data.bat
@@ -4,5 +4,5 @@ robocopy C:\Data\eventstore \\tsclient\F\data_from_production *.chk
 REM 2) copy indexes
 robocopy C:\Data\eventstore\index \\tsclient\F\data_from_production\index /mir
 
-REM 3) copy all historical chunks
+REM 3) copy all historical chunks that are read-only, which ignores the currently-active one. Just remove the /ia:R to get that one since the default behavior is to ignore files that have not been changed
 robocopy C:\Data\eventstore \\tsclient\F\data_from_production chunk*.* /ia:R
