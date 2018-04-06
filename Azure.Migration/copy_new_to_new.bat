dd15a073cb9e82373480eb3e009355d28bd64f5d
Josh Gough <jsgough@gmail.com>
2018-04-06 17:53:02 -0400

    S-64842 Flipped SRC and DEST for consistency

--- Azure.Migration/copy_new_to_new.bat
+++ Azure.Migration/copy_new_to_new.bat
@@ -1,2 +1,2 @@
 REM 1) copy data from one production node to another
-robocopy F:\data_from_production \\tsclient\F\data_from_production /mir
+robocopy \\tsclient\F\data_from_production F:\data_from_production /mir
