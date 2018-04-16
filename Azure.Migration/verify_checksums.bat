9df5ad1801f12e713db765ccd4a4e43a2a555c93
Josh Gough <jsgough@gmail.com>
2018-04-15 22:41:04 -0400

    S-64842 Renamed to clarify purposes for chunks only verification

--- Azure.Migration/verify_checksums.bat
+++
@@ -1,2 +0,0 @@
-REM verify previously-generated checksums
-C:\CommitStream\dependencies\bin\fciv -v -sha1 -xml checksums.xml
---
+++ Azure.Migration/verify_checksums_chunks.bat
@@ -0,0 +1,2 @@
+REM verify previously-generated checksums
+C:\CommitStream\dependencies\bin\fciv -v -sha1 -xml checksums-chunks.xml
